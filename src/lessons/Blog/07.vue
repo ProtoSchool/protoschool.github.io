5269dddbbbed73449811ff3eab454e7a94be186a
Volker Mische <volker.mische@gmail.com>
2018-07-27 14:19:57 +0200

    fix: improve blog lesson 7
    It has slighly better instructions now and valiation

--- src/lessons/Blog/07.vue
+++ src/lessons/Blog/07.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="lesson-blog-07">
-    <Lesson v-bind:text="text" v-bind:code="code" :validate="validate"
+    <Lesson v-bind:text="text" v-bind:code="code"
+            :validate="validate"
+            :modules="modules"
             lessonTitle="Generate the overview page">
     </Lesson>
   </div>
@@ -9,9 +11,17 @@
 <script>
 import Lesson from '../../components/Lesson'
 import text from './07.md'
-// const CID = require('cids')
+import utils from './utils.js'
+const shallowEqualArrays = require('shallow-equal/arrays')
+const CID = require('cids')
 
 const code = `/* globals ipfs */
+
+const CID = require('cids')
+
+const traversePosts = async (cid) => {
+ // Your code goes here
+}
 
 const run = async () => {
   const natCid = await ipfs.dag.put({author: "Nat"})
@@ -67,18 +77,63 @@ const run = async () => {
     ]
   })
 
-  return dogPostCid
+  return traversePosts
 }
 
 return run`
+
+// eslint-disable-next-line no-unused-vars
+const _solution = `
+const traversePosts = async (cid) => {
+  const result = []
+  while (cid) {
+    result.push(cid)
+    const current = await ipfs.dag.get(cid)
+    const prev = current.value.prev
+    if (prev) {
+      cid = new CID(prev['/'])
+    } else {
+      return result
+    }
+  }
+}
+`
 
 const validate = async (result, ipfs) => {
   if (!result) {
     return {fail: 'You forgot to return a result :)'}
   }
-  // TODO vmx 2018-07-25 proper validation:
+  if (typeof result !== 'function') {
+    return {fail: 'Return value needs to be a function.'}
+  }
+
+  const treePostCid = 'zdpuAyYnsUYhTSyqGEEsR6nnexB9xoqvHuKU5HPSuzv5G9hcc'
+  const computerPostCid = 'zdpuAzfNY2rjMLNHPk1neuLQg9cGrpk7WAdE3uHx4gvTHYw1A'
+  const dogPostCid = 'zdpuAoxgoh78gWs4mdCF9is7yWiZJ1rUztLWcs4VssAXLi644'
+  try {
+    const returnValue = await result(new CID(dogPostCid))
+    if (returnValue === undefined) {
+      return {fail: `Your function needs to return 3 CIDs.`}
+    }
+    if (returnValue.length !== 3) {
+      return {fail: `Your function needs to return 3 CIDs.`}
+    }
+    const isCids = returnValue.every(CID.isCID)
+    if (!isCids) {
+      return {fail: `Your function needs to return CIDs`}
+    }
+    const expectedCids = [treePostCid, computerPostCid, dogPostCid]
+    const returnedCids = returnValue.map((item) => new CID(item).toBaseEncodedString())
+    if (!shallowEqualArrays(returnedCids.sort(), expectedCids.sort())) {
+      return {fail: `The CIDs returned by the function ${utils.stringify(returnedCids)} did not match the the expected CIDs ${utils.stringify(expectedCids)}.`}
+    }
+  } catch (err) {
+    return {fail: `Your function cannot be executed with an error: ${err}.`}
+  }
   return {success: 'All works!'}
 }
+
+let modules = {cids: require('cids')}
 
 export default {
   components: {
@@ -88,7 +143,8 @@ export default {
     return {
       code,
       text,
-      validate
+      validate,
+      modules
     }
   }
 }
