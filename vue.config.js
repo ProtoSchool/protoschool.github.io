1c5c4058fa35455587daf08eb05430006fc2dc72
Steven Allen <steven@stebalien.com>
2018-08-02 16:24:51 -0700

    use relative paths

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,12 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 module.exports = {
+  baseUrl: './',
   configureWebpack: config => {
     config.plugins.push(
       new CopyWebpackPlugin([
         {
           from: 'node_modules/monaco-editor/min/vs',
-          to: 'vs',
+          to: 'vs'
         }
       ])
     )
