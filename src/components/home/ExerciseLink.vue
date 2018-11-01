7d17654de720b97b07406fd243e1eca515153b36
terichadbourne <terichad@gmail.com>
2018-11-01 12:50:52 -0400

    Relabel lessons and exercises
    - On the homepage (in `ExerciseLink.vue`), list the parts of each
    workshop as "Lesson X" rather than "Exercise X"
    - In each exercise box (in `Lesson.vue`), change "Exercise X" to
    "Exercise"
    - Addresses issue #22

--- src/components/home/ExerciseLink.vue
+++ src/components/home/ExerciseLink.vue
@@ -1,6 +1,6 @@
 <template>
   <router-link :to="to" class="link db pa3 bb b--white green hover-bg-washed-yellow">
-    <span class="green ttu f6 pr3">Exercise {{index}}</span>
+    <span class="green ttu f6 pr3">Lesson {{index}}</span>
     <span class="navy fw5">{{name}}</span>
   </router-link>
 </template>
