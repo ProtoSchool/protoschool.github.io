d2cbb1788749f2f74d74dbae557515c6947c9c29
terichadbourne <terichad@gmail.com>
2018-11-27 14:41:35 -0500

    Reformat trophy placement in lesson list
    - Reformat exercise list to put emoji at left with even spacing

--- src/components/home/ExerciseLink.vue
+++ src/components/home/ExerciseLink.vue
@@ -1,9 +1,12 @@
 <template>
   <router-link :to="to" class="link db pa3 bb b--white green hover-bg-washed-yellow">
     <div class="flex">
+      <div  class="w2">
+        <span v-if="lessonPassed(to)">&#127942;</span>
+      </div>
       <div class="green ttu f6 pr3" style="min-width: 83px">Lesson {{index}}</div>
       <div class="navy fw5 mw6">{{name}}</div>
-      <div  v-if="lessonPassed(to)"> &#127942;</div>
+
 
     </div>
   </router-link>
