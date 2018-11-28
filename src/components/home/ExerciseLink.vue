f757e479eecefba2478bd02bb5ab7e0d1dd47a3d
terichadbourne <terichad@gmail.com>
2018-11-28 11:29:01 -0500

    Add new completion status icons to homepage
    - Add new icons for lesson status: not-started, in-progress, completed
    - Apply `completed` icon when the lessonPassed is true and `not-started`
    otherwise
    - TODO: Figure out the logic needed to judge whether a lesson has been
    started and not completed and apply the `in-progress` icon

--- src/components/home/ExerciseLink.vue
+++ src/components/home/ExerciseLink.vue
@@ -1,10 +1,11 @@
 <template>
   <router-link :to="to" class="link db pa3 bb b--white green hover-bg-washed-yellow">
     <div class="flex">
-      <div  class="w2">
-        <span v-if="lessonPassed('passed' + to)">&#127942;</span>
+      <div class="green ttu f6" style="min-width: 72px">Lesson {{index}}</div>
+      <div class="pr2">
+        <img v-if="lessonPassed('passed' + to)" src="./complete.svg" alt="complete" style="height: 1rem;"/>
+        <img v-else src="./not-started.svg" alt="not yet started" style="height: 1rem;"/>
       </div>
-      <div class="green ttu f6 pr3" style="min-width: 83px">Lesson {{index}}</div>
       <div class="navy fw5 mw6">{{name}}</div>
     </div>
   </router-link>
