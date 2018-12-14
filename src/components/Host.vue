e73d1aae70cf9f9f5987d78b6d9534ae46db5b7d
terichadbourne <terichad@gmail.com>
2018-12-14 10:20:11 -0500

    Update content on Host page
    - Update `Host` page content to clarify what a chapter is and mention
    other ways to contribute

--- src/components/Host.vue
+++ src/components/Host.vue
@@ -3,20 +3,38 @@
     <Header/>
     <div class="home">
       <section class="db">
-        <h1 class="tc">Organize Your Own Chapter Events</h1>
-        <p class="f4 fw5 lh-copy ma0 pv4 indent-1 measure-wide">
+        <h1 class="tc">Organize Chapter Events</h1>
+        <p class="f4 fw5 lh-copy ma0 pv3 indent-1 measure-wide">
         ProtoSchool is a community-driven effort and is 100% open source. This
         means that anyone can use the ProtoSchool materials and/or host an event.
         <p>
-        <p class="f4 fw5 lh-copy ma0 pv4 indent-1 measure-wide">
-        The only expectations for event hosts is that events be not-for-profit
-        (free or low cost) and that governance of events should have an open
-        membership model (so that nobody can 'control' the events).
+        <p class="f4 fw5 lh-copy ma0 pv3 indent-1 measure-wide">
+        Events are run independently by local <router-link to="/chapters">
+        ProtoSchool chapters</router-link> around the world, but we ask that all
+        events be not-for-profit (free or low-cost) and that governance of events
+        have an open membership model (so that nobody can "control" the events).
         <p>
-        <p class="f4 fw5 lh-copy ma0 pv4 indent-1 measure-wide">
+        <p class="f4 fw5 lh-copy ma0 pv3 indent-1 measure-wide">
+        ProtoSchool chapters also have an online presence to keep local
+        communities interacting between events. Each chapter has its own
+        GitHub repo within our organization, which makes it easy to manage a
+        discussion board using GitHub issues and host a chapter website on our
+        domain using GitHub Pages.
+        <p>
+        <p class="f4 fw5 lh-copy ma0 pv3 indent-1 measure-wide">
         Check out our <a href="https://github.com/protoschool/organizing">
         Organizing repo</a> for info on how to create a chapter, run a
         successful event, and build a local ProtoSchool community!
+        </p>
+        <h2 class="indent-1">Other ways to contribute</h2>
+        <p class="f4 fw5 lh-copy ma0 pv3 indent-1 measure-wide">
+        Not ready to host a chapter quite yet? We'd love your help
+        <router-link to="/build">building new workshops</router-link>,
+        improving existing ones, answering technical questions, or
+        making our documentation more clear.
+        Learn more about the many
+        <a href="https://github.com/ProtoSchool/organizing/blob/master/CONTRIBUTING.md#ways-to-contribute">
+        ways to contribute</a>.
         </p>
       </section>
     </div>
