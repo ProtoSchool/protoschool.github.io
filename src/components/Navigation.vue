7051c34be0245fb305aaf6ac8daf85a1131f3312
terichadbourne <terichad@gmail.com>
2019-01-11 11:55:17 -0500

    Create hamburger menu
    - Create hamburger menu on narrower screen widths
    - Pair programmed with @olizilla
    - TODO: On mobile nav, replace "Current Page" with appropriate route

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,19 +1,43 @@
 <template>
-  <section class="flex items-center bg-aqua white pv3"
-    <div class="bg-aqua w-100">
-      <nav v-if="isLesson" class="flex items-center bg-aqua navy pv3 center tc mw7">
-        <router-link class="nav-link navy" :to="'/'">Home</router-link> >
-        <router-link class="nav-link navy" :to="'/tutorials'">Tutorials</router-link> >
-        <span class="fake-nav-link">{{workshopShortname}}</span>
-      </nav>
-      <nav v-else class="flex items-center bg-aqua white pv3 center tc mw7">
-        <div v-for="link in links">
-          <router-link v-if="link.path === $route.path"
-           class="nav-link white" :to="`${link.path}`">{{link.text}}</router-link>
-           <router-link v-else
-            class="nav-link navy" :to="`${link.path}`">{{link.text}}</router-link>
+  <section>
+    <div class="dn db-ns bg-aqua pv3">
+      <div class="center mw7">
+        <!-- nav if in lesson (breadcrumbs) -->
+        <nav v-if="isLesson" class="flex overflow-auto items-center bg-aqua navy pv3 center tc mw7">
+          <router-link class="nav-link navy" :to="'/'">Home</router-link> >
+          <router-link class="nav-link navy" :to="'/tutorials'">Tutorials</router-link> >
+          <span class="fake-nav-link">{{workshopShortname}}</span>
+        </nav>
+        <!-- standard nav  -->
+          <nav v-else class="dn flex overflow-auto items-center bg-aqua white pv3 center tc mw7">
+            <div v-for="link in links">
+              <router-link v-if="link.path === $route.path"
+               class="nav-link white" :to="`${link.path}`">{{link.text}}</router-link>
+               <router-link v-else
+                class="nav-link navy" :to="`${link.path}`">{{link.text}}</router-link>
+            </div>
+          </nav>
         </div>
-      </nav>
+    </div>
+    <div class="db dn-ns">
+      <div class="flex items-center bg-aqua pv3 w-100">
+        <div class="flex-auto link pa2 fw5 f5 db bb border-aqua white">Current Page</div>
+        <button v-on:click="toggleHamburger" class="button-reset bg-transparent b--transparent">
+          <img v-if="isHamburgerClosed" src="../images/burger.svg"/>
+          <img v-else src="../images/close.svg"/>
+        </button>
+      </div>
+        <!-- mobile nav that expands and hides -->
+        <div v-bind:class="{ dn: isHamburgerClosed }">
+          <nav class="tc bg-aqua-muted white">
+            <div v-for="link in links">
+              <router-link v-on:click.native="toggleHamburger" v-if="link.path === $route.path"
+               class="link pa3 fw5 f4 db bb border-aqua white" :to="`${link.path}`">{{link.text}}</router-link>
+               <router-link v-else
+                class="link pa3 fw5 f4 db bb border-aqua navy" :to="`${link.path}`">{{link.text}}</router-link>
+            </div>
+          </nav>
+      </div>
     </div>
   </section>
 </template>
@@ -24,6 +48,7 @@ export default {
   name: 'Navigation',
   data: self => {
     return {
+      isHamburgerClosed: true,
       currentPath: self.$route.path.toString(),
       workshopShortname: (self.$route.path.charAt(1).toUpperCase() + self.$route.path.slice(2, self.$route.path.lastIndexOf('/'))).split('-').join(" "),
       links: [
@@ -68,15 +93,16 @@ export default {
           return false
         }
       }
+    },
+    methods: {
+      toggleHamburger: function () {
+        this.isHamburgerClosed = !this.isHamburgerClosed
+      }
     }
 }
 </script>
 
 <style scoped>
-
-nav {
-  overflow: auto;
-}
 
 .nav-link {
   cursor: pointer;
@@ -101,7 +127,7 @@ nav {
 
 
 
-@media screen and (max-width: 617px) {
+/* @media screen and (max-width: 617px) {
   nav {
     flex-direction: column;
   }
@@ -109,6 +135,6 @@ nav {
     display: block;
     margin: 5px 5px;
   }
-}
+} */
 
 </style>
