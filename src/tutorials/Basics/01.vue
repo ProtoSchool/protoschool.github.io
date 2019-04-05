b94d66811a1ce77795c75ede1dd9c0820def59da
Diogo Silva <fsdiogo@gmail.com>
2019-04-05 15:46:58 +0100

    fix: expected result of  Lesson 3

--- src/tutorials/Basics/01.vue
+++ src/tutorials/Basics/01.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="lesson-01">
-    <Lesson :text="text" :validate="validate"
-            :exercise="exercise" :concepts="concepts"
-            lessonTitle="Create a node and return a Content Identifier (CID)">
-    </Lesson>
-  </div>
+  <Lesson
+    :text="text"
+    :validate="validate"
+    :exercise="exercise"
+    :concepts="concepts"
+    lessonTitle="Create a node and return a Content Identifier (CID)">
+  </Lesson>
 </template>
 
 <script>
@@ -12,24 +13,26 @@ import Lesson from '../../components/Lesson'
 import text from './01.md'
 import concepts from './01-concepts.md'
 import exercise from './01-exercise.md'
-const CID = require('cids')
+import CID from 'cids'
 
 const validate = async (result, ipfs) => {
   if (!result) {
-    return {fail: 'You forgot to return a result :)'}
+    return { fail: 'You forgot to return a result :)' }
   }
+
   if (!CID.isCID(result)) {
-    return {fail: 'Did not return a valid CID instance.'}
+    return { fail: 'Did not return a valid CID instance.' }
   }
-  let hash = 'zdpuApm55Qv2zRxb3KKjVUxhQSw2r4a7jeiDV7163yCQnfivy'
+
+  const hash = 'zdpuApm55Qv2zRxb3KKjVUxhQSw2r4a7jeiDV7163yCQnfivy'
   if (result.toBaseEncodedString() === hash) {
-    return {success: 'Everything works!'}
+    return { success: 'Everything works!' }
   } else {
-    let obj = await ipfs.dag.get(result)
-    let expected = JSON.stringify({test: 1})
-    let got = JSON.stringify(obj.value)
-    let fail = `Was expecting "${expected}" but got "${got}"`
-    return {fail}
+    const obj = await ipfs.dag.get(result)
+    const expected = JSON.stringify({ test: 1 })
+    const got = JSON.stringify(obj.value)
+
+    return { fail: `Was expecting "${expected}" but got "${got}"` }
   }
 }
 
--- src/tutorials/Basics/02.vue
+++ src/tutorials/Basics/02.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="lesson-02">
-    <Lesson v-bind:text="text" v-bind:code="code"
-            :validate="validate"
-            :modules="modules"
-            :exercise="exercise"
-            :concepts="concepts"
-            lessonTitle="Create a new node that's linked to an old one">
-    </Lesson>
-  </div>
+  <Lesson
+    :text="text"
+    :code="code"
+    :validate="validate"
+    :modules="modules"
+    :exercise="exercise"
+    :concepts="concepts"
+    lessonTitle="Create a new node that's linked to an old one">
+  </Lesson>
 </template>
 
 <script>
@@ -15,46 +15,47 @@ import Lesson from '../../components/Lesson'
 import text from './02.md'
 import concepts from './02-concepts.md'
 import exercise from './02-exercise.md'
-const CID = require('cids')
+import CID from 'cids'
 
 let code = `/* globals ipfs */
 
 const run = async () => {
-  let cid = await ipfs.dag.put({test: 1})
+  let cid = await ipfs.dag.put({ test: 1 })
   // your code goes here
 }
 
 return run
 `
+
 const validate = async (result, ipfs) => {
   if (!result) {
-    return {fail: 'You forgot to return a result :)'}
+    return { fail: 'You forgot to return a result :)' }
   }
+
   if (!CID.isCID(result)) {
-    return {fail: 'Did not return a valid CID instance.'}
+    return { fail: 'Did not return a valid CID instance.' }
   }
-  let hash = 'zdpuAoPanArLvuFtuvmLYuSvp8zE8wuKSMZUkMN8Y1PaHLvKP'
+
+  const hash = 'zdpuAoPanArLvuFtuvmLYuSvp8zE8wuKSMZUkMN8Y1PaHLvKP'
   if (result.toBaseEncodedString() === hash) {
-    return {success: 'Everything works!'}
+    return { success: 'Everything works!' }
   } else {
-    let obj = await ipfs.dag.get(result)
-    let expected = JSON.stringify({bar: new CID(hash)})
-    let got = JSON.stringify(obj.value)
-    let fail = `Was expecting "${expected}" but got "${got}"`
-    return {fail}
+    const obj = await ipfs.dag.get(result)
+    const expected = JSON.stringify({ bar: new CID(hash) })
+    const got = JSON.stringify(obj.value)
+
+    return { fail: `Was expecting "${expected}" but got "${got}"` }
   }
 }
 
-let modules = {cids: require('cids')}
+const modules = { cids: require('cids') }
 
 export default {
   components: {
     Lesson
   },
   data: () => {
-    return {
-      code, text, validate, modules, exercise, concepts
-    }
+    return { code, text, validate, modules, exercise, concepts }
   }
 }
 </script>
