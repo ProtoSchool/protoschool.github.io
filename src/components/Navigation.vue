36699967c1d346cd8855e26689afa5afcd333773
Diogo Silva <fsdiogo@gmail.com>
2019-04-10 11:02:14 +0100

    chore: add navbar link to landing (#189)
    Multiple times I've found myself wanting to click the marked link to go to the tutorial landing page, but that wasn't a link.
    
    <img width="903" alt="nav" src="https://user-images.githubusercontent.com/33324750/55732468-ef77e000-5a13-11e9-8d3e-7be1b60fd585.png">
    
    ✅ It is now!

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -3,45 +3,44 @@
     <!-- STANDARD NAV -->
     <div class="dn db-ns bg-aqua pv3">
       <div class="center mw7">
-        <!-- nav if in lesson (breadcrumbs) -->
-        <div v-if="isLesson" class="flex overflow-auto items-center bg-aqua navy pv3 center tc mw7">
-          <router-link class="nav-link navy" :to="'/tutorials'">Tutorials </router-link> >
-          <span class="fake-nav-link">{{workshopShortname}}</span>
+        <!-- If in lesson (breadcrumbs) -->
+        <div v-if="isLesson" class="flex overflow-auto items-center bg-aqua navy f5 fw6 pv3 center tc mw7">
+          <router-link class="nav-link navy" to="/tutorials">Tutorials</router-link>
+          <span class="fw4">></span>
+          <router-link class="nav-link navy" :to="workshopLanding">{{workshopShortname}}</router-link>
         </div>
         <!-- standard nav  -->
-          <div v-else class="dn flex overflow-auto items-center bg-aqua white pv3 center tc mw7">
-            <div v-for="link in links">
-              <router-link v-if="link.path === $route.path"
-               class="nav-link white" :to="`${link.path}`">{{link.text}}</router-link>
-               <router-link v-else
-                class="nav-link navy" :to="`${link.path}`">{{link.text}}</router-link>
-            </div>
+        <div v-else class="dn flex overflow-auto items-center bg-aqua white pv3 center tc mw7">
+          <div v-for="link in links">
+            <router-link :class="[isActive(link) ? 'white' : 'navy ', 'nav-link']" :to="`${link.path}`">{{link.text}}</router-link>
           </div>
         </div>
+      </div>
     </div>
+
     <!-- MOBILE NAV -->
     <div class="db dn-ns">
       <div class="flex items-center bg-aqua pv3 w-100">
+        <!-- If in lesson (breadcrumbs) -->
         <div v-if="isLesson" class="flex-auto link pa2 fw5 f5 db bb border-aqua navy">
-          <router-link class="link navy" :to="'/tutorials'">Tutorials</router-link> >
-          <span class="white">{{workshopShortname}}</span>
+          <router-link class="nav-link navy" to="/tutorials">Tutorials</router-link>
+          <span class="fw4"> > </span>
+          <router-link class="nav-link navy" :to="workshopLanding">{{workshopShortname}}</router-link>
         </div>
+        <!-- standard nav  -->
         <div v-else class="flex-auto link pa2 fw5 f5 db bb border-aqua white">{{currentPage}}</div>
-        <button v-on:click="toggleHamburger" class="button-reset bg-transparent b--transparent pr2">
+        <button @click="toggleHamburger" class="button-reset bg-transparent b--transparent pr2">
           <img v-if="isHamburgerClosed" src="../static/images/burger.svg"/>
           <img v-else src="../static/images/close.svg"/>
         </button>
       </div>
-        <!-- hamburger displayed when requested -->
-        <div v-bind:class="{ dn: isHamburgerClosed }">
-          <div class="tc bg-aqua-muted white">
-            <div v-for="link in links">
-              <router-link v-on:click.native="toggleHamburger" v-if="link.path === $route.path"
-               class="link pa3 fw5 f4 db bb border-aqua white" :to="`${link.path}`">{{link.text}}</router-link>
-               <router-link v-else
-                class="link pa3 fw5 f4 db bb border-aqua navy" :to="`${link.path}`">{{link.text}}</router-link>
-            </div>
+      <!-- hamburger displayed when requested -->
+      <div :class="{ dn: isHamburgerClosed }">
+        <div class="tc bg-aqua-muted white">
+          <div v-for="link in links">
+            <router-link @click.native="toggleHamburger" :class="[isActive(link) || isActiveLesson(link) ? 'white' : 'navy', 'link pa3 fw5 f4 db bb border-aqua']" :to="`${link.path}`">{{link.text}}</router-link>
           </div>
+        </div>
       </div>
     </div>
   </nav>
@@ -51,36 +50,19 @@
 
 export default {
   name: 'Navigation',
-  data: self => {
+  data: (self) => {
     return {
       isHamburgerClosed: true,
       currentPath: self.$route.path.toString(),
-      workshopShortname: (self.$route.path.charAt(1).toUpperCase() + self.$route.path.slice(2, self.$route.path.lastIndexOf('/'))).split('-').join(' '),
+      workshopShortname: self.$route.path.split('/')[1].split('-').map(e => e.charAt(0).toUpperCase() + e.slice(1)).join(' '),
+      workshopLanding: `/${self.$route.path.split('/')[1]}`,
       links: [
-        {
-          text: 'Home',
-          path: '/'
-        },
-        {
-          text: 'Tutorials',
-          path: '/tutorials'
-        },
-        {
-          text: 'Chapters',
-          path: '/chapters'
-        },
-        {
-          text: 'Contribute',
-          path: '/contribute'
-        },
-        {
-          text: 'Host',
-          path: '/host'
-        },
-        {
-          text: 'Build',
-          path: '/build'
-        }
+        { text: 'Home', path: '/' },
+        { text: 'Tutorials', path: '/tutorials' },
+        { text: 'Chapters', path: '/chapters' },
+        { text: 'Contribute', path: '/contribute' },
+        { text: 'Host', path: '/host' },
+        { text: 'Build', path: '/build' }
       ]
     }
   },
@@ -92,17 +74,12 @@ export default {
           count++
         }
       })
-      if (count === 0) {
-        return true
-      } else {
-        return false
-      }
+      return count === 0
     },
     currentPage: function () {
       let pageName
       this.links.forEach(link => {
         if (link.path === this.currentPath) {
-          console.log(link.text)
           pageName = link.text.toString()
         }
       })
@@ -112,32 +89,34 @@ export default {
   methods: {
     toggleHamburger: function () {
       this.isHamburgerClosed = !this.isHamburgerClosed
+    },
+    isActive: function (link) {
+      return link.path === this.$route.path
+    },
+    isActiveLesson: function (link) {
+      return this.isLesson && link.path === '/tutorials'
     }
   }
 }
 </script>
 
 <style scoped>
-
 .nav-link {
+  margin: 2px 20px;
+  font-size: 18px;
+  font-weight: 700;
+  text-decoration: none;
   cursor: pointer;
-  font-size: 18px;
-  font-weight: 700;
-  margin: 2px 20px;
-  text-decoration: none;
 }
-.fake-nav-link {
-  font-size: 18px;
-  font-weight: 700;
-  margin: 2px 20px;
-  text-decoration: none;
-  color: white;
-}
-.nav-link:first {
-  margin: 2px 20px 2px 0px;
-}
-.nav-link:focus, .nav-link:hover {
+
+.nav-link:hover {
   color: white;
 }
 
+@media screen and (max-width: 479px) {
+  .nav-link {
+    margin: 2px 0px;
+    font-size: 16px;
+  }
+}
 </style>
