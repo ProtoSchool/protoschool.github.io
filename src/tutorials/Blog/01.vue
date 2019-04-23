76744d95036bcc9ab49ff600dcfa201a63a14677
Diogo Silva <fsdiogo@gmail.com>
2019-04-23 10:27:04 +0100

    fix: code indentation

--- src/tutorials/Blog/01.vue
+++ src/tutorials/Blog/01.vue
@@ -39,8 +39,14 @@ const run = async () => {
   const natCid = await ipfs.dag.put({ author: "Nat" })
   const samCid = await ipfs.dag.put({ author: "Sam" })
 
-  const treePostCid = await ipfs.dag.put({ content: "trees", author: samCid })
-  const computerPostCid = await ipfs.dag.put({ content: "computers", author: natCid })
+  const treePostCid = await ipfs.dag.put({
+    content: "trees",
+    author: samCid
+  })
+  const computerPostCid = await ipfs.dag.put({
+    content: "computers",
+    author: natCid
+  })
 
   return [treePostCid, computerPostCid]
 }
