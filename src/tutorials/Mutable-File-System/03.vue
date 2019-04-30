b786fbbfeabed93d115abe4c5e1d8666d3897c55
terichadbourne <terichad@gmail.com>
2019-04-30 17:24:27 -0400

    Apply new logging feature to file upload demo

--- src/tutorials/Mutable-File-System/03.vue
+++ src/tutorials/Mutable-File-System/03.vue
@@ -16,11 +16,29 @@ import text from './03.md'
 import exercise from './03-exercise.md'
 
 const validate = async (result, ipfs) => {
-  if (!result || typeof result === 'undefined') {
-    return { fail: "Looks like you forgot to return a result. Remember to remove the '//' on Line 5." }
-  } else if (typeof result === 'number') {
-    const fileCount = result > 1 ? `${result} files` : '1 file'
-    return { success: `You successfully uploaded ${fileCount}! Check out your console in the inspector. The last entry there is the \`files\` array your browser now has access to. Click the triangle to expand its contents and see what fields are included.` }
+  if (!result || typeof result.length === 'undefined') {
+    return { fail: `Looks like you forgot to return a result. Did you forget to remove the "//" before "return files"?` }
+  } else if (typeof result.length === 'number') {
+    const fileCount = result.length > 1 ? `${result.length} files` : '1 file'
+    const filesArray = []
+    const files = result
+    for (var i = 0; i < files.length; i++){
+      var file = {
+          'lastModified': files[i].lastModified,
+          'lastModifiedDate': files[i].lastModifiedDate,
+          'name': files[i].name,
+          'size': files[i].size,
+          'type': files[i].type,
+          'webkitRelativePath':  files[i].webkitRelativePath
+      }
+      filesArray.push(file)
+    }
+    const log = JSON.stringify(filesArray, null, 2)
+    return {
+      success: `You successfully uploaded ${fileCount}!`,
+      logDesc: `Check out the data below to see the data now accessible in the "files" array.`,
+      log: log
+    }
   } else {
     return { fail: 'Something is wrong. Reset the code and see the instructions.' }
   }
@@ -29,14 +47,13 @@ const validate = async (result, ipfs) => {
 const code = `const run = async (files) => {
   console.log(files)
   /* remove the '//' on the line below to complete this challenge */
-  // return files.length
+  // return files
 }
 return run
 `
 
 const solution = `const run = async (files) => {
-  console.log(files)
-  return files.length
+  return files
 }
 return run
 `
