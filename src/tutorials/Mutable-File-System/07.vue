0906027dddde21e8218b21326a340f8c8273d3e4
terichadbourne <terichad@gmail.com>
2019-05-03 12:44:51 -0400

    add validation for other errors in mkdir lesson 7

--- src/tutorials/Mutable-File-System/07.vue
+++ src/tutorials/Mutable-File-System/07.vue
@@ -32,7 +32,7 @@ const validate = async (result, ipfs) => {
   // let returnedArray = await ipfs.files.ls('/', {long: true})
 // let directoryContentsMatch = JSON.stringify(result) === JSON.stringify(expected)
 
-  /***** CHECK FOR CORRECT DIRECTORY ******/
+  /***** CHECK FOR CORRECT /SOME/STUFF DIRECTORY ******/
 
   // correct directory hash
   const someStuffHash = 'QmVneuc3suf78aVdvFY3BW9HoiEfNxD7WB5zu1f9fbun3D' // /some/stuff/
@@ -48,54 +48,57 @@ const validate = async (result, ipfs) => {
     }
   }
 
-  console.log('CORRECT contains(someStuffHash, "some"): ', contains(someStuffHash, 'some'))
-  console.log('WRONG contains(emptyDirectoryHash, "some"): ', contains(emptyDirectoryHash, 'some'))
-  console.log('WRONG contains(emptyDirectoryHash, "stuff"): ', contains(emptyDirectoryHash, 'stuff'))
-  console.log('WRONG contains(stuffSomeHash, "stuff"): ', contains(stuffSomeHash, 'stuff'))
-
-
   /****** CHECK FOR CORRECT FILENAMES *****/
 
   // expected filenames
   let uploadedFiles = window.uploadedFiles || false
+  let expectedSorted = null
   if (uploadedFiles) {
     console.log('uploaded files: ', uploadedFiles)
     let expected = uploadedFiles.map( file => file.name.toString() )
     console.log('expected: ', expected)
     console.log(Array.isArray(expected))
     expected.push('some')
-    console.log(expected)
-    let expectedSorted = expected.sort()
-    console.log(expectedSorted)
+    console.log("expected: ", expected)
+    expectedSorted = expected.sort()
+    console.log("expectedSorted: ", expectedSorted)
   } else {
     console.log('no files uploaded')
   }
 
   //let ipfsFilenames = expected.map( file => { file.name.toString() ).sort()
 //  let itemsMatch = JSON.stringify(ipfsFilenames) === JSON.stringify(uploadedFilenames)
-//  let itemsAreFiles = expected.every(file => file.type === 0)
+    let someItemIsFile = result.some(file => file.type === 0)
 //  let rightFilesUploaded = itemsMatch && itemsAreFiles
 
   if (!result) {
     return {fail: 'Oops, you forgot to return a result. Did you accidentally delete `return directoryContents`?'}
   } else if (result && result.error && result.error.message === 'file does not exist') {
     // user forgot to use {parents: true} option so path isn't found
-    return { fail: 'The path to the directory you\'re trying to create can\'t be found. Did you forget to use the \{ parents: true \} option?'}
+    return { fail: 'The path to the directory you\'re trying to create can\'t be found. Did you forget to use the {parents: true} option?'}
   } else if (uploadedFiles = false) {
     // shouldn't happen because you can't hit submit without uploading files
     return {fail: 'Oops! You forgot to upload files to work with :('}
-  //} else if (expected.length === 0) {
-    // if no files are written to IPFS because they change the default code
-//    return {fail: 'Uh oh. Looks like no files made it into IPFS. Did you accidentally edit the default `write` code?'}
+  } else if (!someItemIsFile) {
+    // no files written to IPFS because user changed default code to remove write command
+    return {
+      fail: 'Uh oh. Looks like no files made it into IPFS. Did you accidentally edit the default `write` code?',
+      logDesc: "Here's what's in your root directory:",
+      log: log
+    }
   } else if (result[0].hash.length === 0) {
-    return {fail: 'Oops! Looks like you forgot to use the {long: true} option! Open your console to see what happened.'}
+    // user edited the ls command to remove {long: true}
+    return {
+      fail: 'Oops! Looks like you edited the `ls` command and forgot to use the {long: true} option. Check out the results below, then try again without touching that part of the code.',
+      logDesc: "Here's what happened when you forgot to use {long: true}:",
+      log: log
+      }
   } else if (contains(emptyDirectoryHash, 'some')) {
     return { fail: 'Uh oh. Looks like you created /some instead of /some/stuff.' }
   } else if (contains(stuffSomeHash, 'stuff')) {
     return { fail: 'Uh oh. Looks like you created /stuff/some instead of /some/stuff.' }
   } else if (contains(emptyDirectoryHash, 'stuff')) {
     return { fail: 'Uh oh. Looks like you created /stuff instead of /some/stuff.' }
-
   } else if (!contains(someStuffHash, 'some')) {
     return { fail: 'Uh oh. Looks like your directory doesn\'t contain an empty /some/stuff/ directory' }
   } else if (contains(someStuffHash, 'some')){
