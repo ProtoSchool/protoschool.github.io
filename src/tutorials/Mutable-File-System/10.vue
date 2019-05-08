3314cbf831de3d4744063592bc7a3da90f363a58
terichadbourne <terichad@gmail.com>
2019-05-08 19:30:46 -0400

    first draft of files.read lesson

--- src/tutorials/Mutable-File-System/10.vue
+++ src/tutorials/Mutable-File-System/10.vue
@@ -6,6 +6,7 @@
     :modules="modules"
     :exercise="exercise"
     :solution="solution"
+    :errorOverride="true"
     lessonTitle="Read the contents of a file" />
 </template>
 
@@ -15,31 +16,61 @@ import text from './10.md'
 import exercise from './10-exercise.md'
 
 const validate = async (result, ipfs) => {
-  if (result) {
-    return { success: 'Happy Message!' }
+  console.log('result is:', result)
+  console.log(typeof result)
+  if (!result) {
+    return { fail: 'You forgot to return a result. Did you accidentally edit the return statement?'}
+  } else if (result.error && result.error.message === 'Unexpected token return'){
+    return { fail: 'Hmm. That doesn\'t look right. Did you forget to set the value of secretMessage?'}
+  } else if (result && typeof result !== 'string') {
+    return { fail: 'Oops. `secretMessage` should be a string. Did you forget to convert the buffer to a string?' }
+  } else if (result && typeof result === 'string') {
+    return {
+      success: 'You did it!',
+      logDesc: 'Here\'s the secret message you discovered in `success.txt`:',
+      log: result
+    }
+  } else if (result.error) {
+    return { error: result.error }
   } else {
     return { fail: 'Sad but useful message :(' }
   }
 
-  /*
-    There are some additional options you can find useful:
-
-    If you want to show some data or result to the user, it's possible to add an additional step after submitting the code:
-    https://github.com/ProtoSchool/protoschool.github.io/blob/code/README.md#display-results-to-the-user-optional
-
-    If you want to catch external errors and override them to display a more user-friendly error message:
-    https://github.com/ProtoSchool/protoschool.github.io/blob/code/README.md#override-external-error-messages-optional
-  */
 }
 
-const code = `const run = async (files) => {
-  // Your code here
+
+const code = `/* global ipfs */
+  const run = async (files) => {
+  await Promise.all(files.map(f => ipfs.files.write('/' + f.name, f, {create: true})))
+  await ipfs.files.mkdir('/some/stuff', { parents: true })
+  let rootDirectoryContents = await ipfs.files.ls('/', {long: true})
+  rootDirectoryContents.forEach(item => {
+    if (item.type === 0) { ipfs.files.mv('/' + item.name, '/some/stuff') }
+  })
+  ipfs.files.cp('/ipfs/Qme1zmi8dxBiVM7K9y5J3oPxiWWBgzA7n9M6tkmkz8kSwV', '/some/stuff')
+  let someStuffDirectoryContents = await ipfs.files.ls('/some/stuff', {long: true})
+
+  let secretMessage = // your code goes here
+
+  return secretMessage
 }
 return run
 `
 
-const solution = `const run = async (files) => {
-  // Your solution here
+const solution = `/* global ipfs */
+  const run = async (files) => {
+  await Promise.all(files.map(f => ipfs.files.write('/' + f.name, f, {create: true})))
+  await ipfs.files.mkdir('/some/stuff', { parents: true })
+  let rootDirectoryContents = await ipfs.files.ls('/', {long: true})
+  rootDirectoryContents.forEach(item => {
+    if (item.type === 0) { ipfs.files.mv('/' + item.name, '/some/stuff') }
+  })
+  ipfs.files.cp('/ipfs/Qme1zmi8dxBiVM7K9y5J3oPxiWWBgzA7n9M6tkmkz8kSwV', '/some/stuff')
+  let someStuffDirectoryContents = await ipfs.files.ls('/some/stuff', {long: true})
+
+  let secretMessage = (await ipfs.files.read('/some/stuff/success.txt')).toString('utf8')
+
+  return secretMessage
 }
 return run
 `
