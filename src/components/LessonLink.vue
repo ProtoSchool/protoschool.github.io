83e25353e2cf5a41439087ec5bc8fc530c6c135d
terichadbourne <terichad@gmail.com>
2019-05-08 10:31:33 -0400

    fix alt tag for in progress symbol

--- src/components/LessonLink.vue
+++ src/components/LessonLink.vue
@@ -4,7 +4,7 @@
       <div class="green ttu f6" style="min-width: 72px">Lesson {{index}}</div>
       <div class="pr2">
         <img v-if="lessonPassed('passed' + to)" src="../static/images/complete.svg" alt="complete" style="height: 1rem;"/>
-        <img v-else-if="lessonCached('cached' + to)" src="../static/images/in-progress.svg" alt="complete" style="height: 1rem;"/>
+        <img v-else-if="lessonCached('cached' + to)" src="../static/images/in-progress.svg" alt="in progress" style="height: 1rem;"/>
         <img v-else src="../static/images/not-started.svg" alt="not yet started" style="height: 1rem;"/>
       </div>
       <div class="navy fw5 mw6">{{name}}</div>
