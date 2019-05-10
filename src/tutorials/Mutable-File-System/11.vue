4cb1298082a14369a9c7e589e63f837d5b251059
terichadbourne <terichad@gmail.com>
2019-05-10 12:45:55 -0400

    add initial validation for lesson 11 rm

--- src/tutorials/Mutable-File-System/11.vue
+++ src/tutorials/Mutable-File-System/11.vue
@@ -6,7 +6,8 @@
     :modules="modules"
     :exercise="exercise"
     :solution="solution"
-    lessonTitle="Remove a file" />
+    :overrideErrors="true"
+    lessonTitle="Remove a file or directory" />
 </template>
 
 <script>
@@ -15,31 +16,77 @@ import text from './11.md'
 import exercise from './11-exercise.md'
 
 const validate = async (result, ipfs) => {
-  if (result) {
-    return { success: 'Happy Message!' }
-  } else {
-    return { fail: 'Sad but useful message :(' }
+  const rootDirectoryContents = await ipfs.files.ls('/', { long: true })
+  console.log('rootDirectoryContents: ', rootDirectoryContents)
+  const rootDirectoryStatus = await ipfs.files.stat('/')
+  const correctHash = rootDirectoryStatus.hash === "QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn"
+
+  console.log('rootDirectoryStatus: ', rootDirectoryStatus)
+  console.log(rootDirectoryStatus.hash)
+
+
+  if (!result) {
+    return { fail: 'Oops! You forgot to return a result :(' }
+  } else if (result.error && result.error.message === '/some is a directory, use -r to remove directories') {
+    return { fail: 'Oops! You tried to remove `/some` but it didn\'t work because you forgot to use `{ recursive: true }`.' }
+  } else if (result.error && result.error.message === '/some/stuff is a directory, use -r to remove directories'){
+    return { fail: 'Oops! You tried to remove `/some/stuff` but it didn\'t work because you forgot to use `{ recursive: true }`. Try again and give some thought to which directory you should remove.' }
+  } else if (!rootDirectoryStatus.hash) {
+    return { fail: 'Your root directory doesn\'t look right. Are you sure you ran the `files.rm` method on your `/some` directory with the option `{ recursive: true }`?' }
+  } else if (!correctHash) {
+    // only removed /some/stuff with {recursive:true} or tried to remove the root itself
+    return {
+      fail: 'Your root directory isn\'t empty. Are you sure you ran the `files.rm` method on your `/some` directory with the option `{ recursive: true }`?',
+      logDesc: 'Here are the current contents of your root directory:',
+      log: JSON.stringify(rootDirectoryContents, null, 2)
+    }
+  } else if (correctHash) {
+    return {
+      success: 'Success! You\'ve completed this series of lessons! Be sure to check out the cool tidbit below before moving on.',
+      logDesc: "Your function returned an empty array ( `[]` ) as the contents of your root directory. Its status (`stat`) is shown below. Note that we're back to exactly the same CID we started with!",
+      log: JSON.stringify(rootDirectoryStatus, null, 2)
+    }
+  } else if (result.error) {
+    return { error: result.error }
   }
-
-  /*
-    There are some additional options you can find useful:
-
-    If you want to show some data or result to the user, it's possible to add an additional step after submitting the code:
-    https://github.com/ProtoSchool/protoschool.github.io/blob/code/README.md#display-results-to-the-user-optional
-
-    If you want to catch external errors and override them to display a more user-friendly error message:
-    https://github.com/ProtoSchool/protoschool.github.io/blob/code/README.md#override-external-error-messages-optional
-  */
+  // Output the default error if we haven't caught any
+  return { error: result.error }
 }
 
-const code = `const run = async (files) => {
-  // Your code here
+const code = `/* global ipfs */
+  const run = async (files) => {
+  await Promise.all(files.map(f => ipfs.files.write('/' + f.name, f, {create: true})))
+  await ipfs.files.mkdir('/some/stuff', { parents: true })
+  let rootDirectoryContents = await ipfs.files.ls('/', {long: true})
+  const filepathsToMove = rootDirectoryContents.filter(file => file.type === 0).map(file => '/' + file.name)
+  await ipfs.files.mv(filepathsToMove, '/some/stuff')
+  await ipfs.files.cp('/ipfs/Qme1zmi8dxBiVM7K9y5J3oPxiWWBgzA7n9M6tkmkz8kSwV', '/some/stuff')
+  let someStuffDirectoryContents = await ipfs.files.ls('/some/stuff', {long: true})
+  let secretMessage = (await ipfs.files.read('/some/stuff/success.txt')).toString('utf8')
+
+  // your code goes here
+
+  let finalRootDirectoryContents = await ipfs.files.ls('/', {long: true})
+  return finalRootDirectoryContents
 }
 return run
 `
 
-const solution = `const run = async (files) => {
-  // Your solution here
+const solution = `/* global ipfs */
+  const run = async (files) => {
+  await Promise.all(files.map(f => ipfs.files.write('/' + f.name, f, {create: true})))
+  await ipfs.files.mkdir('/some/stuff', { parents: true })
+  let rootDirectoryContents = await ipfs.files.ls('/', {long: true})
+  const filepathsToMove = rootDirectoryContents.filter(file => file.type === 0).map(file => '/' + file.name)
+  await ipfs.files.mv(filepathsToMove, '/some/stuff')
+  await ipfs.files.cp('/ipfs/Qme1zmi8dxBiVM7K9y5J3oPxiWWBgzA7n9M6tkmkz8kSwV', '/some/stuff')
+  let someStuffDirectoryContents = await ipfs.files.ls('/some/stuff', {long: true})
+  let secretMessage = (await ipfs.files.read('/some/stuff/success.txt')).toString('utf8')
+
+  await ipfs.files.rm('/some', { recursive: true })
+
+  let finalRootDirectoryContents = await ipfs.files.ls('/', {long: true})
+  return finalRootDirectoryContents
 }
 return run
 `
