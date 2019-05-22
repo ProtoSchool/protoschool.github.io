b547b15e83ae0e897e01cf22f565987897256ed8
Diogo Silva <fsdiogo@gmail.com>
2019-05-22 16:12:55 +0100

    feat: make lesson 10 work

--- src/tutorials/Mutable-File-System/10-exercise.md
+++ src/tutorials/Mutable-File-System/10-exercise.md
@@ -1,5 +1,3 @@
-We hid a secret message for you in that file you copied from IPFS as `success.txt`. Use `files.read` to save the content of the file (as a string) to the `secretMessage` variable we've created for you below.
+We hid a secret message for you in that file you copied from IPFS as `QmWCscor6qWPdx53zEQmZvQvuWQYxx1ARRCXwYVE4s9wzJ`. Use `files.read` to save the content of the file (as a string) to the `secretMessage` variable we've created for you below.
 
 **Hint:** Remember that you'll need to convert the buffer returned by `files.read` to a string.
-
-**NOTE: Validation isn't yet complete for this lesson. Need to comment out the line with the CP command and upload a `success.txt` file insted because earlier cp lesson doesn't work yet.**
--- src/tutorials/Mutable-File-System/10.vue
+++ src/tutorials/Mutable-File-System/10.vue
@@ -7,6 +7,7 @@
     :exercise="exercise"
     :solution="solution"
     :errorOverride="true"
+    :createTestFile="true"
     lessonTitle="Read the contents of a file" />
 </template>
 
@@ -16,64 +17,56 @@ import text from './10.md'
 import exercise from './10-exercise.md'
 
 const validate = async (result, ipfs) => {
-  console.log(err)
-  console.log('result is:', result)
-  console.log(typeof result)
   if (!result) {
-    return { fail: 'You forgot to return a result. Did you accidentally edit the return statement?'}
-  } else if (result === '[object Promise]') {
-    // this one isn't working
-    return { fail: 'Something went wrong. Did you try to turn your buffer into a string but forget to wrap your async function in parentheses first?'}
-  } else if (result.error && result.error.message === 'Unexpected token return'){
-    return { fail: 'Hmm. That doesn\'t look right. Did you forget to set the value of secretMessage?'}
+    return { fail: 'You forgot to return a result. Did you accidentally edit the return statement?' }
   } else if (result && typeof result !== 'string') {
     return { fail: 'Oops. `secretMessage` should be a string. Did you forget to convert the buffer to a string?' }
-  } else if (result && typeof result === 'string' && result !== '[object Promise]') {
+  } else if (result && typeof result === 'string') {
     return {
-      success: 'You did it!',
-      logDesc: 'Here\'s the secret message you discovered in `success.txt`:',
+      success: 'Success!',
+      logDesc: "Here's the secret message you discovered in the file:",
       log: result
     }
   } else if (result.error) {
     return { error: result.error }
-  } else {
-    return { fail: 'Sad but useful message :(' }
   }
-
 }
 
-
 const code = `/* global ipfs */
-  const run = async (files) => {
-  await Promise.all(files.map(f => ipfs.files.write('/' + f.name, f, {create: true})))
+
+const run = async (files) => {
+  await Promise.all(files.map(f => ipfs.files.write('/' + f.name, f, { create: true })))
   await ipfs.files.mkdir('/some/stuff', { parents: true })
-  let rootDirectoryContents = await ipfs.files.ls('/', {long: true})
+  let rootDirectoryContents = await ipfs.files.ls('/', { long: true })
   const filepathsToMove = rootDirectoryContents.filter(file => file.type === 0).map(file => '/' + file.name)
   await ipfs.files.mv(filepathsToMove, '/some/stuff')
-  ipfs.files.cp('/ipfs/Qme1zmi8dxBiVM7K9y5J3oPxiWWBgzA7n9M6tkmkz8kSwV', '/some/stuff')
-  let someStuffDirectoryContents = await ipfs.files.ls('/some/stuff', {long: true})
+  ipfs.files.cp('/ipfs/QmWCscor6qWPdx53zEQmZvQvuWQYxx1ARRCXwYVE4s9wzJ', '/some/stuff')
+  let someStuffDirectoryContents = await ipfs.files.ls('/some/stuff', { long: true })
 
   let secretMessage = // your code goes here
 
   return secretMessage
 }
+
 return run
 `
 
 const solution = `/* global ipfs */
-  const run = async (files) => {
+
+const run = async (files) => {
   await Promise.all(files.map(f => ipfs.files.write('/' + f.name, f, {create: true})))
   await ipfs.files.mkdir('/some/stuff', { parents: true })
   let rootDirectoryContents = await ipfs.files.ls('/', {long: true})
   const filepathsToMove = rootDirectoryContents.filter(file => file.type === 0).map(file => '/' + file.name)
   await ipfs.files.mv(filepathsToMove, '/some/stuff')
-  ipfs.files.cp('/ipfs/Qme1zmi8dxBiVM7K9y5J3oPxiWWBgzA7n9M6tkmkz8kSwV', '/some/stuff')
-  let someStuffDirectoryContents = await ipfs.files.ls('/some/stuff', {long: true})
+  ipfs.files.cp('/ipfs/QmWCscor6qWPdx53zEQmZvQvuWQYxx1ARRCXwYVE4s9wzJ', '/some/stuff')
+  let someStuffDirectoryContents = await ipfs.files.ls('/some/stuff', { long: true })
 
-  let secretMessage = (await ipfs.files.read('/some/stuff/success.txt')).toString('utf8')
+  let secretMessage = (await ipfs.files.read('/some/stuff/QmWCscor6qWPdx53zEQmZvQvuWQYxx1ARRCXwYVE4s9wzJ')).toString('utf8')
 
   return secretMessage
 }
+
 return run
 `
 
