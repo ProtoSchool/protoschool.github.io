faea0b74437e76d652920ad4d096fcc119575418
Diogo Silva <fsdiogo@gmail.com>
2019-06-27 16:36:34 +0100

    fix: mobile icon sizes (#246)

--- src/components/LessonLink.vue
+++ src/components/LessonLink.vue
@@ -3,9 +3,9 @@
     <div class="flex">
       <div class="tc green ttu f6" style="min-width: 92px">Lesson {{index}}</div>
       <div class="pr3">
-        <img v-if="lessonPassed('passed' + to)" src="../static/images/complete.svg" alt="complete" style="height: 1rem;"/>
-        <img v-else-if="lessonCached('cached' + to)" src="../static/images/in-progress.svg" alt="in progress" style="height: 1rem;"/>
-        <img v-else src="../static/images/not-started.svg" alt="not yet started" style="height: 0.9rem;"/>
+        <img v-if="lessonPassed('passed' + to)" src="../static/images/complete.svg" alt="complete" style="height: 1rem; max-width: 1rem;"/>
+        <img v-else-if="lessonCached('cached' + to)" src="../static/images/in-progress.svg" alt="in progress" style="height: 1rem; max-width: 1rem;"/>
+        <img v-else src="../static/images/not-started.svg" alt="not yet started" style="height: 0.9rem; max-width: 1rem;" />
       </div>
       <div class="navy fw5 mw6">{{name}}</div>
     </div>
