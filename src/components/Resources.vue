c199d5387351de4df56929f0aa44183b509d2c62
Diogo Silva <fsdiogo@gmail.com>
2019-07-09 15:10:50 +0100

    fix: ui

--- src/components/Resources.vue
+++ src/components/Resources.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="lesson-text lh-copy">
-    <div v-for="(item, idx) in data" :key="`resources-${idx}`" class="mb2">
-      <p class="mb2">
+    <div v-for="(item, idx) in data" :key="`resources-${idx}`" class="mb3">
+      <p class="ma0 flex items-center">
         <a class="b blue link" :href="item.link" target="_blank">{{item.title}}</a>
         <span class="ml2 ph1 bg-navy br-pill white f7">{{item.type}}</span>
       </p>
