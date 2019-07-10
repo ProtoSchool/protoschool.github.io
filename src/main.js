70531f796d8abc9a7518610e351aaf5445d4c104
Diogo Silva <fsdiogo@gmail.com>
2019-07-10 14:53:59 +0100

    fix: resources link

--- src/main.js
+++ src/main.js
@@ -63,13 +63,13 @@ const routes = [
   { path: '/data-structures/03', component: LessonDataStructures03 },
   { path: '/data-structures/04', component: LessonDataStructures04 },
   { path: '/data-structures/05', component: LessonDataStructures05 },
-  { path: '/data-structures', component: ResourcesLesson, props: { tutorialId: 'dataStructures' } },
+  { path: '/data-structures/resources', component: ResourcesLesson, props: { tutorialId: 'dataStructures' } },
   // Lessons - Basics
   { path: '/basics', component: Landing, props: { tutorialId: 'basics' } },
   { path: '/basics/01', component: LessonBasics01 },
   { path: '/basics/02', component: LessonBasics02 },
   { path: '/basics/03', component: LessonBasics03 },
-  { path: '/basics', component: ResourcesLesson, props: { tutorialId: 'basics' } },
+  { path: '/basics/resources', component: ResourcesLesson, props: { tutorialId: 'basics' } },
   // Lessons - Blog
   { path: '/blog', component: Landing, props: { tutorialId: 'blog' } },
   { path: '/blog/01', component: LessonBlog01 },
@@ -79,7 +79,7 @@ const routes = [
   { path: '/blog/05', component: LessonBlog05 },
   { path: '/blog/06', component: LessonBlog06 },
   { path: '/blog/07', component: LessonBlog07 },
-  { path: '/blog', component: ResourcesLesson, props: { tutorialId: 'blog' } },
+  { path: '/blog/resources', component: ResourcesLesson, props: { tutorialId: 'blog' } },
   // Lessons - MFS
   { path: '/mutable-file-system', component: Landing, props: { tutorialId: 'mutableFileSystem' } },
   { path: '/mutable-file-system/01', component: MutableFileSystem01 },
@@ -93,7 +93,7 @@ const routes = [
   { path: '/mutable-file-system/09', component: MutableFileSystem09 },
   { path: '/mutable-file-system/10', component: MutableFileSystem10 },
   { path: '/mutable-file-system/11', component: MutableFileSystem11 },
-  { path: '/mutable-file-system', component: ResourcesLesson, props: { tutorialId: 'mutableFileSystem' } },
+  { path: '/mutable-file-system/resources', component: ResourcesLesson, props: { tutorialId: 'mutableFileSystem' } },
   // 404
   { path: '*', name: '404' }
 ]
