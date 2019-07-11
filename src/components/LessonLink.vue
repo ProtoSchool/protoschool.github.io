6c97a13a032939485fe9efb8c5bec695be8eeb97
Diogo Silva <fsdiogo@gmail.com>
2019-07-11 18:19:00 +0100

    fix: mobile icons (#256)
    * fix: mobile icons
    
    * untruncate

--- src/components/LessonLink.vue
+++ src/components/LessonLink.vue
@@ -3,7 +3,7 @@
     <div class="flex items-center">
       <div v-if="isResources" class="tc green ttu f6" style="min-width: 93px">Resources</div>
       <div v-else class="tc green ttu f6" style="min-width: 93px">Lesson {{index}}</div>
-      <div class="pr2 flex">
+      <div class="pr2" style="flexShrink: 0">
         <img v-if="lessonPassed('passed' + to)" src="../static/images/complete.svg" alt="complete" style="height: 1rem;"/>
         <img v-else-if="lessonCached('cached' + to)" src="../static/images/in-progress.svg" alt="in progress" style="height: 1rem;"/>
         <img v-else src="../static/images/not-started.svg" alt="not yet started" style="height: 0.9rem;"/>
@@ -23,7 +23,7 @@ export default {
   ],
   computed: {
     isResources: function () {
-      return this.to.split("/")[2] === 'resources'
+      return this.to.split('/')[2] === 'resources'
     }
   },
   methods: {
