0bf37ab56e371714d9f021716415692517128bdd
Teri Chadbourne <terichadbourne@users.noreply.github.com>
2019-07-16 21:15:27 -0400

    Fix mailto link (#271)
    * Fix mailto link 
    
    🤦🏼‍♀️
    
    * remove anchor tag

--- src/pages/Contribute.vue
+++ src/pages/Contribute.vue
@@ -61,7 +61,6 @@
         ways to contribute</a>.
         </p>
 
-        <a name="contact"></a> 
         <h2>Get in Touch</h2>
         <p class="f4 fw5 lh-copy ma0 pv3 ">
         We use GitHub to organize ProtoSchool. The best place to get in touch
@@ -72,7 +71,7 @@
         improving the ProtoSchool website or curriculum, please open an issue in the
         <a href="https://github.com/ProtoSchool/protoschool.github.io" target="blank">
         website repository</a>. For other issues, including Code of Conduct reports, 
-        please [email the project maintainers](mailto:protoschool@protocol.ai).
+        please <a href="mailto:protoschool@protocol.ai">email the project maintainers</a>.
         </p>
 
         <h2>Code of Conduct</h2>
