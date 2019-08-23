a48d39405595dc4cd9388a55d129ea6483ef58f4
terichadbourne <terichad@gmail.com>
2019-08-23 14:26:00 -0400

    update 404 page formatting

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -2,9 +2,9 @@
   <div>
     <Header :notFound="notFound" />
     <section class="mw7 center ph2">
-      <h1 class="mt5">Page not found</h1>
-      <p class="f4 fw5 lh-copy ma0 pv2">It seems like you hit a route that doesn't exist 😢</p>
-      <p class="f4 fw5 lh-copy ma0 pt2">Go to the <router-link to="/">homepage</router-link> or check out our awesome <router-link to="/tutorials">tutorials</router-link>!</p>
+      <h1 class="mt5">Page Not Found</h1>
+      <p class="f4 fw5 lh-copy ma0 pv3">Oops! Looks like you've tried to access a page that doesn't exist. 😢  </p>
+      <p class="f4 fw5 lh-copy ma0 pv3">May we suggest perusing our <router-link to="/">homepage</router-link> or checking out our awesome collection of <router-link to="/tutorials">interactive tutorials</router-link>?</p>
     </section>
   </div>
 </template>
@@ -21,9 +21,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-h1 {
-  font-size: 6rem;
-}
-</style>
