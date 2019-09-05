d5505e28172cef4cc55a9e053800d8231028bcd9
Daniela Borges Matos de Carvalho <alunassertiva@gmail.com>
2019-09-05 17:20:38 +0100

    Fix small issue in the exercise code (#294)
    There was a missing `await`, which I believe was not on purpose

--- src/tutorials/0004/10.vue
+++ src/tutorials/0004/10.vue
@@ -47,7 +47,7 @@ const run = async (files) => {
   let rootDirectoryContents = await ipfs.files.ls('/', { long: true })
   const filepathsToMove = rootDirectoryContents.filter(file => file.type === 0).map(file => '/' + file.name)
   await ipfs.files.mv(filepathsToMove, '/some/stuff')
-  ipfs.files.cp('/ipfs/QmWCscor6qWPdx53zEQmZvQvuWQYxx1ARRCXwYVE4s9wzJ', '/some/stuff/success.txt')
+  await ipfs.files.cp('/ipfs/QmWCscor6qWPdx53zEQmZvQvuWQYxx1ARRCXwYVE4s9wzJ', '/some/stuff/success.txt')
   let someStuffDirectoryContents = await ipfs.files.ls('/some/stuff', { long: true })
 
   let secretMessage = // your code goes here
@@ -61,7 +61,7 @@ return run
 const solution = `/* global ipfs */
 
 const run = async (files) => {
-  await Promise.all(files.map(f => ipfs.files.write('/' + f.name, f, {create: true})))
+  await Promise.all(files.map(f => ipfs.files.write('/' + f.name, f, { create: true })))
   await ipfs.files.mkdir('/some/stuff', { parents: true })
   let rootDirectoryContents = await ipfs.files.ls('/', { long: true })
   const filepathsToMove = rootDirectoryContents.filter(file => file.type === 0).map(file => '/' + file.name)
