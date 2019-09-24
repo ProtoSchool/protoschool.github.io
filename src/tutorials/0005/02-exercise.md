07788f6efd3ebb33cff216bec2a3636d561c9ff7
Gil Domingues <gil.domingues2@hotmail.com>
2019-09-24 16:42:36 +0100

    feat: draft of the second lesson done. also bumped ipfs version

---
+++ src/tutorials/0005/02-exercise.md
@@ -0,0 +1,2 @@
+Let's try to add a file to your IPFS node! First, try to upload one or more files.
+
---
+++ src/tutorials/0005/02.vue
@@ -0,0 +1,130 @@
+<template>
+  <FileLesson
+    :text="text"
+    :code="code"
+    :validate="validate"
+    :overrideErrors="true"
+    :modules="modules"
+    :exercise="exercise"
+    :solution="solution" />
+</template>
+
+<script>
+import pTimeout from 'p-timeout'
+import FileLesson from '../../components/FileLesson'
+import text from './02.md'
+import exercise from './02-exercise.md'
+
+const code = `/* global ipfs */
+const run = async (files) => {
+  const result = []
+  
+  for(const file of files) {
+    const afterAdd = // Place your code to add a file here
+
+    result.push(afterAdd)
+  }
+
+  return result
+}
+return run
+`
+
+const solution = `/* global ipfs */
+const run = async (files) => {
+  const result = []
+  
+  for(const file of files) {
+    const afterAdd = await ipfs.add(file)
+
+    result.push(afterAdd)
+  }
+
+  return result
+}
+return run
+`
+
+const validate = async (result, ipfs) => {
+  // Learn about working with uploaded files:
+  // https://github.com/ProtoSchool/protoschool.github.io/README.md#work-with-uploaded-files-for-file-upload-lessons-only
+
+  // TODO Validate it is an array of objects and not an array of promises (no .then())
+  // TODO Check the number of uploaded files is the same of nodes in the result
+
+  const uploadedFiles = window.uploadedFiles || false
+
+  if (!result) {
+    return {
+      fail: 'Oops! You forgot to return a result :('
+    }
+  }
+
+  if (result.length > uploadedFiles.length) {
+    return {
+      fail: 'The array you returned has more items than the number of files you uploaded, did you add something in the array twice.'
+    }
+  }
+
+  if (result.length < uploadedFiles.length) {
+    return {
+      fail: 'The array you returned has less items than the number of files you uploaded, maybe you forgot to put one of the results in the array.'
+    }
+  }
+
+  if (result.error) {
+    return { error: result.error }
+  }
+
+  for (const resultData of result) {
+    if (!Array.isArray(resultData) ||
+      resultData.length !== 1 ||
+      !resultData[0].hash) {
+      return {
+        fail: 'The value you returned is incorrect :( Are you sure you are returning an array of the results of the ipfs.add operations?'
+      }
+    }
+
+    const lsResult = await pTimeout(ipfs.ls(resultData[0].hash), 2000).catch(() => 'error')
+    if (lsResult === 'error' || resultData[0].hash !== lsResult[0].hash) {
+      return {
+        fail: 'The value you returned is incorrect :( Are you sure you are returning an array of the results of the ipfs.add operations?'
+      }
+    }
+  }
+
+  return {
+    success: 'Success! You did it!',
+    logDesc: "Here's the result of adding the files to your IPFS node",
+    log: result
+  }
+
+  /*
+    There are some additional options you can find useful:
+
+    If you want to show some data or result to the user, it's possible to add an additional step after submitting the code:
+    https://github.com/ProtoSchool/protoschool.github.io/blob/code/README.md#display-results-to-the-user-optional
+
+    If you want to catch external errors and override them to display a more user-friendly error message:
+    https://github.com/ProtoSchool/protoschool.github.io/blob/code/README.md#override-external-error-messages-optional
+  */
+}
+
+const modules = { cids: require('cids') }
+
+export default {
+  components: {
+    FileLesson
+  },
+  data: () => {
+    return {
+      text,
+      exercise,
+      code,
+      solution,
+      validate,
+      modules
+    }
+  }
+}
+</script>
