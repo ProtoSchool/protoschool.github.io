daec1f2c9f4a394236395ecf7ae961f93d073fe2
Gil Domingues <gil.domingues2@hotmail.com>
2019-10-02 15:43:57 +0100

    chore: first draft of the 5th lesson

--- src/main.js
+++ src/main.js
@@ -50,6 +50,7 @@ import T0005L01 from './tutorials/0005/01.vue'
 import T0005L02 from './tutorials/0005/02.vue'
 import T0005L03 from './tutorials/0005/03.vue'
 import T0005L04 from './tutorials/0005/04.vue'
+import T0005L05 from './tutorials/0005/05.vue'
 
 Vue
   .use(VueRouter)
@@ -108,6 +109,7 @@ const routes = [
   { path: '/file-api/02', component: T0005L02 },
   { path: '/file-api/03', component: T0005L03 },
   { path: '/file-api/04', component: T0005L04 },
+  { path: '/file-api/05', component: T0005L05 },
   // 404
   { path: '*', name: '404', component: NotFound, props: { notFound: true } }
 ]
---
+++ src/tutorials/0005/05-exercise.md
@@ -0,0 +1,3 @@
+In the previous exercise, we uploaded multiple files into a `/dir` directory. This time, let's check which files are, in fact, in the `/dir` directory of our ipfs node.
+
+**Hint:** The ipfs path (`CID`) you need to call the `ls` function is stored in the `pathCID` variable.
