846673a949c23500be3a9c37861caebe565304bf
Gil Domingues <gil.domingues2@hotmail.com>
2019-11-12 11:10:39 +0000

    chore: remove mention of infinite loop from message. links now appear correctly on errors

--- src/components/Output.vue
+++ src/components/Output.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="pt2">
-    <div v-if="output.test.error" class="lh-copy pv2 ph3 bg-red white">
-      Error: {{output.test.error.message}}
+    <div v-if="output.test.error" class="lh-copy pv2 ph3 bg-red white" v-html="parseData(`Error: ${output.test.error.message}`)">
     </div>
     <div
       v-if="output.test.fail"
