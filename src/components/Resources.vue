58397b1a10a158a89b18f1068c3b7f4ddfe41332
Teri Chadbourne <terichadbourne@users.noreply.github.com>
2020-01-08 06:25:35 -0500

    Add video from IPFS Camp Core Course D to resources pages (#336)
    * add IPFS Camp Core Course D to resources
    
    * enable markdown formatting for resource descriptions
    
    * text edits
    
    * fix css selector, linting errors
    
    * fix issue caused by linter
    
    * revert css edit

--- src/components/Resources.vue
+++ src/components/Resources.vue
@@ -1,21 +1,41 @@
 <template>
-  <div data-cy="resources-content" class="lesson-text lh-copy">
-    <p>Ready to learn more? There are plenty of additional resources to explore, both in ProtoSchool and beyond. <span v-if="data.length > 1">Here are some of our favorites:</span> <span v-else>Here's our top pick:</span></p>
-    <div v-for="(item, idx) in data" :key="`resources-${idx}`" class="mb3">
-      <p class="ma0 flex items-center">
-        <a class="b blue link" :href="item.link" target="_blank">{{item.title}}</a>
-        <span class="ml2 ph2 bg-navy br-pill white f7">{{item.type}}</span>
-        <span v-if="item.link.includes('/proto.school/')" class="ml2 ph2 bg-aqua br-pill white f7">ProtoSchool</span>
+  <div data-cy='resources-content' class='lesson-text lh-copy'>
+    <p>
+      Ready to learn more? There are plenty of additional resources to explore, both in ProtoSchool and beyond.
+      <span v-if='data.length > 1'>Here are some of our favorites:</span>
+      <span v-else>Here's our top pick:</span>
+    </p>
+    <div v-for='(item, idx) in data' :key='`resources-${idx}`' class='mb3'>
+      <p class='ma0 flex items-center'>
+        <a class='b blue link' :href='item.link' target='_blank'>{{item.title}}</a>
+        <span class='ml2 ph2 bg-navy br-pill white f7'>{{item.type}}</span>
+        <span
+          v-if='item.link.includes("/proto.school/")'
+          class='ml2 ph2 bg-aqua br-pill white f7'
+        >ProtoSchool</span>
       </p>
-      <p v-if="item.description" class="ma0">{{item.description}}</p>
+      <div v-if='item.description' class='ma0 resource-desc' v-html='parse(item.description)'></div>
     </div>
   </div>
 </template>
 
 <script>
+import marked from 'marked';
+
 export default {
   props: {
     data: Array
+  },
+  methods: {
+    parse: function(description) {
+      return marked(description || '');
+    }
   }
-}
+};
 </script>
+
+<style>
+.resource-desc > p {
+  margin-top: 0;
+}
+</style>
