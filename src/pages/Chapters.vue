8639ce6f1cf61265359b614b09e90a610f29e0c7
terichadbourne <terichad@gmail.com>
2020-02-03 16:04:08 -0500

    text edits

--- src/pages/Chapters.vue
+++ src/pages/Chapters.vue
@@ -2,22 +2,22 @@
   <div>
     <Header :hidden="hidden"/>
     <section class="mw7 center ph2">
-      <h1 class="mt5">ProtoSchool Chapters</h1>
+      <h1 class="mt5">ProtoSchool Chapters (Archived)</h1>
         <p class="f4 fw5 lh-copy ma0 pv3  ">
         We recently switched from a chapter-based community model to an event-based model to
         create a more scalable system that encourages ProtoSchool content to be presented
-         either one-off or embedded in a variety of exiting educational groups,
+         either one-off or embedded in a variety of existing educational groups,
          with a lower barrier to entry for event leaders. If you previously attended
          events through a ProtoSchool chapter, you'll now find their upcoming
          ProtoSchool workshops listed on our <router-link to="/events">events page</router-link>,
           including links to learn about other content the group might offer.
       </p>
 
-      <router-link to="/events" class="f5 link dim br-pill ph3 pv2 mb4 dib white bg-navy mr3">View all upcoming ProtoSchool events!</router-link>
-
+      <router-link to="/events" class="f5 link dim br-pill ph3 pv2 mb4 dib white bg-navy mr3">View Upcoming Events</router-link>
+      <router-link class="f5 link dim br-pill ph3 pv2 mb2 dib white bg-navy mv3" to="/host">Host an Event</router-link>
 
       <h2>Archived Chapter Listings</h2>
-
+      <p class="f4 fw5 lh-copy ma0 pv3 i "> This is a historical list of ProtoSchool chapters created in 2019 before we changed our community model. Please refer to our <router-link to="/events">events page</router-link> for the most accurate information on upcoming events and the groups hosting them. </p>
       <ul>
         <li v-for="(region, idx) in regions" :key="`region-${idx}`">
           <h3>{{region}}</h3>
