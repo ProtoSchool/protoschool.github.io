91991275d7cf660a5528bb60d6a403134f23a9f8
terichadbourne <terichad@gmail.com>
2020-02-19 15:57:40 -0500

    fix isMultipleChoiceLesson prop -> data

--- src/components/Lesson.vue
+++ src/components/Lesson.vue
@@ -207,7 +207,6 @@ export default {
     validate: Function,
     code: String,
     overrideErrors: Boolean,
-    isMultipleChoiceLesson: Boolean,
     question: String,
     choices: Array,
     createTestFile: Boolean,
@@ -227,6 +226,7 @@ export default {
       expandExercise: false,
       editorReady: false,
       isFileLesson: self.isFileLesson,
+      isMultipleChoiceLesson: self.isMultipleChoiceLesson,
       uploadedFiles: window.uploadedFiles || false,
       choice: localStorage[self.cacheKey] || '',
       cachedChoice: !!localStorage['cached' + self.$route.path]
