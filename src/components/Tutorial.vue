813296799ac4e2792995cf173ae43ffb32d5c48a
Ze Bateira <jlageb@gmail.com>
2020-02-27 15:55:11 +0400

    fix: ui tweak option 1

--- src/components/Tutorial.vue
+++ src/components/Tutorial.vue
@@ -1,14 +1,7 @@
 <template>
   <section class="mw7 w-100 center ph3 flex items-start pv4">
-    <div class="dn db-ns flex-none mr4">
-      <h1 class="ma0 mb2 f3 fw4">{{tutorial.project.name}}</h1>
-      <img
-        :src="tutorial.project.logo"
-        :alt="tutorial.project.name"
-        style="height: 54px" />
-    </div>
     <div class="w-100">
-      <div class="flex justify-between flex-row items-start">
+      <div class="flex justify-between flex-row items-start mb1">
         <div class="flex flex-row items-center">
           <h2 class="ma0 f3 fw5">
             <template v-if="isLanding !== true">
@@ -24,7 +17,15 @@
           :tutorialId="tutorialId"
           class="h2 ml3 type-icon"/>
       </div>
-      <p class="f5 fw5 ma0 mt3 lh-copy charcoal-muted">{{tutorial.description}}</p>
+      <div class="flex items-center">
+        <img
+          :src="tutorial.project.logo"
+          :alt="tutorial.project.name"
+          style="height: 23px"
+        />
+        <h1 class="ml1 mt0 mb0 f6 fw2">{{tutorial.project.name}}</h1>
+      </div>
+      <p class="f5 fw5 ma0 mt4 lh-copy charcoal-muted">{{tutorial.description}}</p>
       <ul class="lessons-list mv4 pa0 f5 br3">
         <template v-for="(lesson, index) in tutorial.lessons">
           <li :key="index">
