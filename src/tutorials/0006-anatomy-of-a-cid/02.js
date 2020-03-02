1e2e6428b83eab3465a9bc98723dd9fc4667eb52
terichadbourne <terichad@gmail.com>
2020-03-02 15:55:06 -0500

    update descriptions of bits/bytes

--- src/tutorials/0006-anatomy-of-a-cid/02.js
+++ src/tutorials/0006-anatomy-of-a-cid/02.js
@@ -2,9 +2,9 @@ const question = 'How do CIDs support multiple cryptographic algorithms?'
 
 const choices = [
   {
-    answer: 'The length of the hash tells us which cryptographic algorithm was used. If the length is `256` then the algorithm used was `sha2-256`. If the length is `512` then the algorithm used was `sha2-512`.',
+    answer: 'The length of the hash tells us which cryptographic algorithm was used. If the length is 256 bits (32 bytes) then the algorithm used was `sha2-256`. If the length is 512 bits (64 bytes) then the algorithm used was `sha2-512`.',
     correct: false,
-    feedback: "It's true that the length of a `sha2-256` hash is `256`, but this isn't enough information to tell us what hashing algorithm was used. Check the information above and try again."
+    feedback: "It's true that the length of a `sha2-256` hash is 256 bits (32 bytes), but this isn't enough information to tell us what hashing algorithm was used. Check the information above and try again."
   },
   {
     answer: 'They prefix the hash with a unique identifier that flags which algorithm was used to generate the hash.',
