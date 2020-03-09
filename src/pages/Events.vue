67d4ca78f3db87dd9fa717e01dd3aa98c4f0ca08
terichadbourne <terichad@gmail.com>
2020-03-09 12:58:29 -0400

    update wording when no local events

--- src/pages/Events.vue
+++ src/pages/Events.vue
@@ -18,11 +18,11 @@
           :future="true"
         />
       </div>
-      <div v-else>
-        <p class="f4 fw5 lh-copy ma0 pv3">More events coming soon!</p>
-      </div>
-      <p class="f4 fw5 lh-copy ma0 pv3">Know of an upcoming workshop not listed here? Ask the event organizer to <a href="https://forms.gle/t1iEzpQAFSYHhpBr9" target="_blank">submit it</a>. </p>
-      <p class="f4 fw5 lh-copy ma0 pv3">Wish there were more workshops near you? Learn how to <a href="https://forms.gle/t1iEzpQAFSYHhpBr9" target="_blank">host an event</a>!</p>
+      <p class="f4 fw5 lh-copy ma0 pv3">
+        <span v-if="futureEvents().length"> <strong>No workshops near you?</strong> Never fear! You </span>
+        <span v-else><strong>More events coming soon!</strong> In the meantime, you </span>
+        can <router-link to="/tutorials">explore our tutorials online</router-link> or learn how to <router-link to="/host">host local events</router-link>. If you know of an upcoming workshop not listed here, please ask the event organizer to <a href="https://forms.gle/t1iEzpQAFSYHhpBr9" target="_blank">submit it</a>.
+      </p>
       <div v-if="pastEvents().length">
         <h2>Past Events
           <span v-if="viewAllPast || (pastEvents().length <= maxRedacted)">(All)</span>
