5921bafefe1aecc3f9d0650dc71785c27d8dc70c
Ze Bateira <jlageb@gmail.com>
2020-04-09 15:59:09 +0100

    docs(module): add some docs to the new settings module

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -1,3 +1,17 @@
+/*
+  Settings module
+
+  Saves the settings in localStorage with the prefix `settings/`.
+  Further settings modules (like filters) should also add a prefix
+  and use constants.
+
+  Examples:
+
+  `settings/filters/*`
+  `settings/newsletters/*`
+  `settings/cookies/*`
+ */
+
 const SETTINGS_KEY_PREFIX = 'settings'
 
 const FILTERS_KEY_PREFIX = 'filters'
