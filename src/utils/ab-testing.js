a55884940ceb825d87d8c6af1a4f51cbf5cfbc6e
Ze Bateira <jlageb@gmail.com>
2020-05-11 15:24:06 +0100

    fix: always log when users are presented with the survey options

--- src/utils/ab-testing.js
+++ src/utils/ab-testing.js
@@ -2,13 +2,8 @@ import { EVENTS } from '../static/countly'
 import settings from './settings'
 
 function testComponent (event) {
-  const cachedOption = settings.abTesting.get(settings.abTesting.TUTORIAL_FEEDBACK_SURVEY)
-
-  if (cachedOption) {
-    return cachedOption
-  }
-
-  const option = Math.random() > 0.5 ? 'optionA' : 'optionB'
+  const option = settings.abTesting.get(settings.abTesting.TUTORIAL_FEEDBACK_SURVEY) ||
+    (Math.random() > 0.5 ? 'optionA' : 'optionB')
 
   window.Countly.q.push(['add_event', {
     key: EVENTS.AB_TESTING.TUTORIAL_FEEDBACK_SURVEY,
