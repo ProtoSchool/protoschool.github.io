ff0a1211c219eb17cab1849af30d43e0ea750d0a
terichadbourne <terichad@gmail.com>
2020-06-08 17:14:21 -0400

    add lesson 3, minor edits to 1, 4

--- src/tutorials/0007-verified-storage-on-filecoin/02.js
+++ src/tutorials/0007-verified-storage-on-filecoin/02.js
@@ -6,17 +6,17 @@ const question = 'When are Proofs of Replication run?'
 // Only one answer can be correct.
 const choices = [
   {
-    answer: 'On a recurring basis over time, at regular intervals.',
+    answer: 'On a recurring basis over time, at regular intervals',
     correct: true,
     feedback: "Nope! We'll learn about other proofs that run multiple times, but a Proof of Replication only needs to run when the data is replicated."
   },
   {
-    answer: 'On demand, whenever a challenge is requested to verify the data.',
+    answer: 'On demand, whenever a challenge is requested to verify the data',
     correct: false,
     feedback: 'Oops! Try again. A Proof of Replication only needs to run when the data is replicated.'
   },
   {
-    answer: 'Only once, at the time you make the storage deal.',
+    answer: 'Only once, at the time you make the storage deal',
     correct: true,
     feedback: "That's right!"
   }
--- src/tutorials/0007-verified-storage-on-filecoin/03.js
+++ src/tutorials/0007-verified-storage-on-filecoin/03.js
@@ -1,25 +1,31 @@
 // Question must be a string
-const question = "What's the meaning of life, the universe, and everything?"
+const question = 'How does the Proof of Storage confirm that given data is stored over time?'
 
 // Choices must be an array of objects, each with the properties:
 // `answer` (string), `correct` (boolean), and `feedback` (string)
 // Only one answer can be correct.
 const choices = [
   {
-    answer: 'Some correct answer.',
+    answer: 'It checks whether data with the right CommD is present on the Filecoin network.',
+    correct: false,
+    feedback: "Nope. Since a CommD is based only on the data and not on its location, a CommD can't help to confirm that a specific miner is honoring their agreement."
+  },
+  {
+    answer: "It re-runs the Proof of Replication at regular intervals to ensure the miner's encoded data still matches the client's original data.",
+    correct: false,
+    feedback: "No. Once the Proof of Replication is complete, we've proven that the encoding is correct, allowing the Proof of Spacetime to rely only on encoded data."
+  },
+  {
+    answer: 'It regularly checks that all encoded data in the CommR is still present on the Filecoin network.',
+    correct: false,
+    feedback: 'While the Proof of Storage does reference the CommR, it only samples a subset of the encoded data.'
+  },
+  {
+    answer: 'It regularly checks to ensure that a random section of encoded data from the CommR is present in the right location.',
     correct: true,
-    feedback: 'Great job!'
-  },
-  {
-    answer: 'Some incorrect answer',
-    correct: false,
-    feedback: "Oops. Here's some clue about why that answer is wrong."
-  },
-  {
-    answer: 'Some incorrect answer',
-    correct: false,
-    feedback: "Sorry, here's some clue about why that answer is wrong."
+    feedback: 'You got it!'
   }
+
 ]
 
 export default {
