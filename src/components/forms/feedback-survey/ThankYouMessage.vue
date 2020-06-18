7f8aaade9910dd1a71ef5913a0a9225cf8f7fe9e
Ze Bateira <jlageb@gmail.com>
2020-06-18 11:22:42 +0100

    feat: add external link option for the button component
    for this we had to split the button into two components: Button and
    ButtonLink.
    
    The normal Button component should be used for situations when we
    need to control the button click action.
    The ButtonLink is simply an anchor tag with the look and feel of the
    button with the new option to be an external link.

--- src/components/forms/feedback-survey/ThankYouMessage.vue
+++ src/components/forms/feedback-survey/ThankYouMessage.vue
@@ -2,26 +2,25 @@
   <div class="thank-you-message">
     <div class="text" v-html="translations.thankYouMessage.text">
     </div>
-    <!-- TODO: show button when survey is available -->
     <div
-      v-if="false"
       class="flex justify-end mt4"
     >
-        <Button
-          link="Tutorials"
+        <ButtonLink
           text="Go to Survey"
+          :link="translations.thankYouMessage.surveyLink"
+          external
         />
     </div>
   </div>
 </template>
 <script>
 import translations from '../../../static/translations'
-import Button from '../../buttons/Button.vue'
+import ButtonLink from '../../buttons/ButtonLink.vue'
 
 export default {
   name: 'ThankYouMessage',
   components: {
-    Button
+    ButtonLink
   },
   data: () => ({
     translations: translations.feedbackSurvey
