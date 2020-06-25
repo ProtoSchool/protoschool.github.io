9cdc1e1aeb11b2c4160ba0b27c63c1ef72d82c10
Ze Bateira <jlageb@gmail.com>
2020-06-25 12:54:44 +0100

    fix: add uncaughtException handler to deal with no config error
    setup BRANCH env var in travis to fix this: if BRANCH=code then we fail
    the script, if not we skip the script.

--- scripts/commands/build-data/index.js
+++ scripts/commands/build-data/index.js
@@ -4,6 +4,22 @@
     - Google APIs for events data
  */
 
+let log = require('npmlog')
+
+const logGroup = 'build:data'
+
+process.on('uncaughtException', (error) => {
+  // We can't fail if the branch is code, because it could be the cron job
+  if (error.code === 'NO_CONFIG' && process.env.BRANCH !== 'code') {
+    log.error(logGroup, 'ERROR NO_CONFIG:', error.message)
+    log.warn(logGroup, 'Not enough config set. Skipping build-data script...')
+    process.exit(0)
+  }
+
+  log.error(logGroup, error.code ? `ERROR ${error.code}:` : '', error.message)
+  process.exit(1)
+})
+
 const run = require('../../modules/run')
 const eventsData = require('./events-data')
 const mailchimp = require('./mailchimp')
