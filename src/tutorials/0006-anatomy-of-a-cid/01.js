a25a699155a5e07d5e78c0c34cfc817371ead872
Ze Bateira <jlageb@gmail.com>
2020-06-25 15:48:26 +0100

    fix: force double quotes on multiple choice files

--- src/tutorials/0006-anatomy-of-a-cid/01.js
+++ src/tutorials/0006-anatomy-of-a-cid/01.js
@@ -1,20 +1,22 @@
-const question = 'What is the default cryptographic algorithm used by IPFS to generate CIDs?'
+/* eslint quotes: ["error", "double"]  */
+
+const question = "What is the default cryptographic algorithm used by IPFS to generate CIDs?"
 
 const choices = [
   {
-    answer: '`sha1`',
+    answer: "`sha1`",
     correct: false,
-    feedback: 'Try again.'
+    feedback: "Try again."
   },
   {
-    answer: '`sha2-256`',
+    answer: "`sha2-256`",
     correct: true,
-    feedback: 'That\'s correct!'
+    feedback: "That's correct!"
   },
   {
-    answer: 'It uses a combination of `sha1`, `sha2-256`, `sha3-256`, `sha3-512` and `shake-256`.',
+    answer: "It uses a combination of `sha1`, `sha2-256`, `sha3-256`, `sha3-512` and `shake-256`.",
     correct: false,
-    feedback: 'IPFS only uses one algorithm to generate a CID.'
+    feedback: "IPFS only uses one algorithm to generate a CID."
   }
 ]
 
