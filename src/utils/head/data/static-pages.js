2e2eaf8e941cca8b0941fb5dbb5eb4b4b9bc9775
Ze Bateira <jlageb@gmail.com>
2020-07-30 19:38:21 +0100

    fix: matching of routes for SEO need have trailing slashes
    netlify always adds a trailing slash to the paths, so we need to
    take that into account.

--- src/utils/head/data/static-pages.js
+++ src/utils/head/data/static-pages.js
@@ -10,68 +10,68 @@ export default {
     'title': 'Homepage',
     'description': 'Interactive tutorials on decentralized web protocols. Explore IPFS and Filecoin through code challenges, code-free lessons, and local events.'
   },
-  '/build': {
+  '/build/': {
     'title': 'Build',
     'og:title': 'Build an Interactive Tutorial for ProtoSchool',
     'description': 'ProtoSchool is a community-driven effort, and we rely on contributors like you to create great educational content!'
   },
-  '/contribute': {
+  '/contribute/': {
     'title': 'Contribute',
     'og:title': 'Contribute to the ProtoSchool Community',
     'description': 'ProtoSchool is a community-driven open source project dedicated to making it easy to get started with decentralized web technologies.'
   },
-  '/events': {
+  '/events/': {
     'title': 'Events',
     'description': 'Live ProtoSchool workshops around the globe offer you the opportunity to complete our interactive tutorials with the support of local mentors.'
   },
-  '/host': {
+  '/host/': {
     'title': 'Host',
     'og:title': 'Organize Local Events with ProtoSchool Tutorials',
     'description': 'ProtoSchool workshops are run independently by groups and inviduals around the world who are excited about introducing others to decentralized web concepts and protocols'
   },
-  '/news': {
+  '/news/': {
     'title': 'News',
     'og:title': 'Subscribe to the ProtoSchool Newsletter',
     'description': 'Subscribe to the ProtoSchool Newsletter for updates on new tutorials and site features.'
   },
 
   // Tutorials Page
-  '/tutorials': {
+  '/tutorials/': {
     'title': 'Tutorials',
     'og:title': 'ProtoSchool Interactive Tutorials',
     'description': `Interactive tutorials on decentralized web protocols. Explore IPFS and Filecoin through code challenges, code-free lessons, and local events.`
   },
-  '/tutorials?code=false': {
+  '/tutorials/?code=false': {
     'og:title': 'ProtoSchool Interactive Code-free Tutorials'
   },
-  '/tutorials?code=false&course=all': {
+  '/tutorials/?code=false&course=all': {
     'og:title': 'ProtoSchool Interactive Code-free Tutorials'
   },
-  '/tutorials?course=ipfs': {
+  '/tutorials/?course=ipfs': {
     'og:title': 'IPFS Interactive IPFS Tutorials'
   },
-  '/tutorials?code=true&course=ipfs': {
+  '/tutorials/?code=true&course=ipfs': {
     'og:title': 'IPFS Interactive IPFS Tutorials'
   },
-  '/tutorials?code=false&course=ipfs': {
+  '/tutorials/?code=false&course=ipfs': {
     'og:title': 'Interactive Code-free IPFS Tutorials'
   },
-  '/tutorials?course=filecoin': {
+  '/tutorials/?course=filecoin': {
     'og:title': 'Interactive Code-free Filecoin Tutorials'
   },
-  '/tutorials?code=true&course=filecoin': {
+  '/tutorials/?code=true&course=filecoin': {
     'og:title': 'Interactive Filecoin Tutorials'
   },
-  '/tutorials?code=false&course=filecoin': {
+  '/tutorials/?code=false&course=filecoin': {
     'og:title': 'Interactive Code-free Filecoin Tutorials'
   },
-  '/tutorials?course=multiformats': {
+  '/tutorials/?course=multiformats': {
     'og:title': 'Interactive Multiformats Tutorials'
   },
-  '/tutorials?code=true&course=multiformats': {
+  '/tutorials/?code=true&course=multiformats': {
     'og:title': 'Interactive Multiformats Tutorials'
   },
-  '/tutorials?code=false&course=multiformats': {
+  '/tutorials/?code=false&course=multiformats': {
     'og:title': 'Interactive Code-free Multiformats Tutorials'
   }
 }
