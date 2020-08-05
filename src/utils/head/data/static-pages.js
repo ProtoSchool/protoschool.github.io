263ee9b16cf61cbb5aa92ff0feb714b064170ddb
terichadbourne <terichad@gmail.com>
2020-08-05 12:06:21 -0400

    edit static page metadata

--- src/utils/head/data/static-pages.js
+++ src/utils/head/data/static-pages.js
@@ -12,66 +12,67 @@ export default {
   },
   '/build/': {
     'title': 'Build',
-    'og:title': 'Build an Interactive Tutorial for ProtoSchool',
-    'description': 'ProtoSchool is a community-driven effort, and we rely on contributors like you to create great educational content!'
+    'og:title': 'Build interactive ProtoSchool tutorials',
+    'description': 'Build interactive ProtoSchool tutorials on decentralized web protocols like IPFS and Filecoin. Create DWeb coding challenges or text-based lessons and quizzes.'
   },
   '/contribute/': {
     'title': 'Contribute',
-    'og:title': 'Contribute to the ProtoSchool Community',
-    'description': 'ProtoSchool is a community-driven open source project dedicated to making it easy to get started with decentralized web technologies.'
+    'og:title': 'Contribute to the ProtoSchool community',
+    'description': 'ProtoSchool is making it easy to get started with decentralized web technologies. Contribute by building tutorials, hosting local events, and more.'
   },
   '/events/': {
-    'title': 'Events',
-    'description': 'Live ProtoSchool workshops around the globe offer you the opportunity to complete our interactive tutorials with the support of local mentors.'
+    'title': 'Events'
+    'og:title': 'Attend ProtoSchool workshops',
+    'description': 'Attend in-person or virtual ProtoSchool workshops around the globe to learn about decentralized web protocols with the support of local mentors.'
   },
   '/host/': {
     'title': 'Host',
-    'og:title': 'Organize Local Events with ProtoSchool Tutorials',
-    'description': 'ProtoSchool workshops are run independently by groups and inviduals around the world who are excited about introducing others to decentralized web concepts and protocols'
+    'og:title': 'Host ProtoSchool workshops',
+    'description': 'Host local ProtoSchool workshops in your communtiy to introduce others to IPFS, Filecoin, and other decentralized web protocols and concepts.'
   },
   '/news/': {
     'title': 'News',
-    'og:title': 'Subscribe to the ProtoSchool Newsletter',
-    'description': 'Subscribe to the ProtoSchool Newsletter for updates on new tutorials and site features.'
+    'og:title': 'Subscribe to the ProtoSchool newsletter',
+    'description': 'Subscribe to the ProtoSchool Newsletter for updates on new tutorials and site features. Opt-in for additional resources for local event leaders.'
   },
 
   // Tutorials Page
   '/tutorials/': {
     'title': 'Tutorials',
-    'og:title': 'ProtoSchool Interactive Tutorials',
-    'description': `Interactive tutorials on decentralized web protocols. Explore IPFS and Filecoin through code challenges, code-free lessons, and local events.`
+    'og:title': `Interactive tutorials on DWeb protocols | ProtoSchool`,
+    'description': `Interactive tutorials on decentralized web protocols. Explore IPFS and Filecoin through coding challenges and code-free tutorials.`
   },
   '/tutorials/?code=false': {
-    'og:title': 'ProtoSchool Interactive Code-free Tutorials'
+    'og:title': 'Code-free decentralized web tutorials at ProtoSchool'
   },
   '/tutorials/?code=false&course=all': {
-    'og:title': 'ProtoSchool Interactive Code-free Tutorials'
+    'og:title': 'Code-free decentralized web tutorials at ProtoSchool'
   },
   '/tutorials/?course=ipfs': {
-    'og:title': 'IPFS Interactive IPFS Tutorials'
+    'og:title': 'IPFS tutorials & coding challenges at ProtoSchool'
   },
   '/tutorials/?code=true&course=ipfs': {
-    'og:title': 'IPFS Interactive IPFS Tutorials'
+    'og:title': 'IPFS tutorials & coding challenges at ProtoSchool'
   },
   '/tutorials/?code=false&course=ipfs': {
-    'og:title': 'Interactive Code-free IPFS Tutorials'
+    'og:title': 'Code-free IPFS tutorials at ProtoSchool'
   },
   '/tutorials/?course=filecoin': {
-    'og:title': 'Interactive Code-free Filecoin Tutorials'
+    'og:title': 'Interactive Filecoin tutorials at ProtoSchool'
   },
   '/tutorials/?code=true&course=filecoin': {
-    'og:title': 'Interactive Filecoin Tutorials'
+    'og:title': 'Interactive Filecoin tutorials at ProtoSchool'
   },
   '/tutorials/?code=false&course=filecoin': {
-    'og:title': 'Interactive Code-free Filecoin Tutorials'
+    'og:title': 'Code-free Filecoin tutorials at ProtoSchool'
   },
   '/tutorials/?course=multiformats': {
-    'og:title': 'Interactive Multiformats Tutorials'
+    'og:title': 'Interactive Multiformats tutorials at ProtoSchool'
   },
   '/tutorials/?code=true&course=multiformats': {
-    'og:title': 'Interactive Multiformats Tutorials'
+    'og:title': 'Interactive Multiformats tutorials at ProtoSchool'
   },
   '/tutorials/?code=false&course=multiformats': {
-    'og:title': 'Interactive Code-free Multiformats Tutorials'
+    'og:title': 'Code-free Multiformats tutorials at ProtoSchool'
   }
 }
