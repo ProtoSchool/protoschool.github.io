c54652333586c4e2c6416da2c06980523dfa43f6
Ze Bateira <jlageb@gmail.com>
2020-08-11 10:53:58 +0100

    fix: remove errors on 404 pages

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -10,7 +10,6 @@ import head from '../utils/head'
 import Header from '../components/Header.vue'
 import Tutorial from '../components/Tutorial.vue'
 import { getTutorialByUrl } from '../utils/tutorials'
-import router from '../router'
 
 export default {
   components: {
@@ -26,15 +25,16 @@ export default {
 
       // If no tutorial was found, redirect to 404 page
       if (!tutorial) {
-        router.replace({ name: '404' })
-        return
+        this.$router.replace({ name: '404' })
+
+        return null
       }
 
       return tutorial
     }
   },
   head () {
-    return head.dynamic.tutorials({ context: this })
+    return this.tutorial && head.dynamic.tutorials({ context: this })
   }
 }
 </script>
