63e0065e6d4135ffd4b7fdf74d084481135ae0ca
terichadbourne <terichad@gmail.com>
2020-09-16 16:36:19 -0400

    add clean quiz.js

---
+++ scripts/commands/wizard/quiz.js
@@ -0,0 +1,17 @@
+// let's create a quiz
+// confirm which tutorial
+// create new tutorial if needed
+// confirm which lesson in tutorial
+// create new lesson if needed
+// confirm overwrite existing quiz if there's real data
+// prompt for question
+// prompt for correct answer (text, success message)
+// prompt repetitively for 2-4 wrong answers (text, failure message)
+// randomly sort order of wrong answers
+// store data to js file (remember to deal with quotes correctly)
+// log randomized questions?
+// show link to preview
+// show which file to go to to edit text
+// create another quiz OR return to main menu for other options OR exit
+
+module.exports = { }
