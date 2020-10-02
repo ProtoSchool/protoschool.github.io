51a20a70c5610111b748c24b7a07caa274dfbffc
terichadbourne <terichad@gmail.com>
2020-10-02 17:01:25 -0400

    exclude specific logs from snapshot

--- jest/helpers/asserts.js
+++ jest/helpers/asserts.js
@@ -1,5 +1,23 @@
+const excludedPhrases = [
+  "Cool! Here's what we get when we mix up the order of the answer choices:"
+]
+
+// returns true if log includes a string from the excludedPhrases array
+function isExcluded (log) {
+  for (let phrase of excludedPhrases) {
+    if (log.includes(phrase)) {
+      return true
+    }
+  }
+  return false
+}
+
+// add log to snapshot UNLESS it contains an excluded phrase
 function assertLogSnapshot (log) {
-  expect(log).toMatchSnapshot()
+  if (!isExcluded(log)) {
+    expect(log).toMatchSnapshot() // expect if log isn't excluded from exertion
+  }
+  // do nothing if log is excluded from assertion
 }
 
 module.exports = { assertLogSnapshot }
