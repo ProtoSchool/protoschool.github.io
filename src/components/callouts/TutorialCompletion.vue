74d26131541f30f2a366607150510893442eb25f
Ze Bateira <jlageb@gmail.com>
2020-11-19 11:06:42 +0000

    feat: add animation on tutorial completion callout dismiss

--- src/components/callouts/TutorialCompletion.vue
+++ src/components/callouts/TutorialCompletion.vue
@@ -1,26 +1,35 @@
 <template>
-  <div v-if="!dismissed" class="tutorial-completion bg-aqua-muted navy pa4 flex f4 br2">
-    <img src="../../static/images/complete.svg" class="w2 h2 mr3" alt="complete" />
-    <div class="flex items-start flex-column">
-      <div class="lh-title">
-        Congratulations on completing the <span class="b">{{tutorial.title}}</span> tutorial!
+  <transition
+    name="state-view-transition"
+  >
+    <div
+      v-if="!dismissed"
+      data-state-view-active="true"
+      data-state-view-transition-function="slide"
+      class="tutorial-completion state-view bg-aqua-muted navy pa4 flex f4 br2"
+    >
+      <img src="../../static/images/complete.svg" class="w2 h2 mr3" alt="complete" />
+      <div class="flex items-start flex-column">
+        <div class="lh-title">
+          Congratulations on completing the <span class="b">{{tutorial.title}}</span> tutorial!
+        </div>
+        <a
+          :href="twitterShareLink"
+          target="_blank"
+          class="twitter-share-link no-underline b navy-muted hover-white inline-flex items-center mt2 pv2 tc br2 link"
+          title="Twitter"
+          v-on:click="dismiss"
+        >
+          <span class="f5 pr2">Share on Twitter</span>
+          <img src="../../static/images/icons/twitter.svg" class="dib h1 w1 navy-muted" alt="twitter" />
+        </a>
       </div>
-      <a
-        :href="twitterShareLink"
-        target="_blank"
-        class="twitter-share-link no-underline b navy-muted hover-white inline-flex items-center mt2 pv2 tc br2 link"
-        title="Twitter"
-        v-on:click="dismiss"
-      >
-        <span class="f5 pr2">Share on Twitter</span>
-        <img src="../../static/images/icons/twitter.svg" class="dib h1 w1 navy-muted" alt="twitter" />
-      </a>
+      <ButtonClose
+        title="Dismiss message"
+        :onDismiss="dismiss"
+      />
     </div>
-    <ButtonClose
-      title="Dismiss message"
-      :onDismiss="dismiss"
-    />
-  </div>
+  </transition>
 </template>
 <script>
 import ButtonClose from '../buttons/ButtonClose'
