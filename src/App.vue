0b4246139c5633c8743ecabf037854a5345b95fe
Zé Bateira <jose.bateira+git@moxy.studio>
2020-11-29 20:04:15 +0000

    fix: make sure the document, body and its children adapt to the viewport (#572)
    footer element on tutorial lessons should go to the bottom of the page,
    and the content should stretch across the entire viewport.

--- src/App.vue
+++ src/App.vue
@@ -16,17 +16,48 @@ export default {
 
 <style>
 
-html {
-  overflow-y: scroll;
+*,
+*::before,
+*::after {
+    box-sizing: border-box;
 }
 
-html, body {
-  min-height: 100%;
+/*
+   Document
+
+   1. Stretch <html> stretch to fill our screen height
+   2. Make children of html (body) occupy at least 100% of the screen
+   3. Viewport is scalable and occupies at least 320px (iPhone SE)
+*/
+
+html {
+    min-width: 25em;
+    height: 0; /* 3 */
+    min-height: 100%; /* 1 */
+    display: flex; /* 2 */
+    flex-direction: column; /* 2 */
+}
+
+/*
+   Body & #app
+
+   1. Force scroll always to prevent scrollbars to appear/disappear based on the page contents
+   2. Make sure that we occupy 100% of our parent and allow our child elements to do the same
+   3. Disable rubber band scrolling
+*/
+
+body {
+    overflow-y: scroll; /* 1 */
+    display: flex; /* 2 */
+    flex: 1 0 auto; /* 2 */
+    flex-direction: column; /* 2 */
 }
 
 #app {
-  width: 100%;
-  height: 100%;
+  display: flex; /* 2 */
+  flex: 1 0 auto; /* 2 */
+  flex-direction: column; /* 2 */
+
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -34,9 +65,9 @@ html, body {
 }
 
 #app > div {
-  min-height: 100%;
-  display: flex;
-  flex-direction: column;
+  display: flex; /* 2 */
+  flex: 1 0 auto; /* 2 */
+  flex-direction: column; /* 2 */
 }
 
 pre {
