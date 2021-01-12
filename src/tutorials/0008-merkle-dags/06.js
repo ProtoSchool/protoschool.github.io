13f8aeff18540dc2b96064a9cb5b10cf66340328
terichadbourne <terichad@gmail.com>
2021-01-12 10:31:23 -0500

    add lesson 6 quiz

--- src/tutorials/0008-merkle-dags/06.js
+++ src/tutorials/0008-merkle-dags/06.js
@@ -3,26 +3,26 @@
 // #PRISTINE# This file was auto-generated. Please remove this line when updating the file manually.
 
 // Question must be a string
-const question = "What's the meaning of life, the universe, and everything?"
+const question = "Which of the following statements about datasets distributed as Merkle DAGs is FASLE?"
 
 // Choices must be an array of objects, each with the properties:
 // `answer` (string), `correct` (boolean), and `feedback` (string)
 // Only one answer can be correct.
 const choices = [
   {
-    answer: "Some correct answer.",
+    answer: "Anyone who has a copy of the dataset&mdash;or a subset of the DAG&mdash;can choose to help distribute it.",
+    correct: false,
+    feedback: "Oops! This statement is actually true. The ability of peers to all distribute the same data is a hallmark of the decentralized web, enabled by content addressing. Can you find the statement that's false?"
+  },
+  {
+    answer: "Because it has a single root node, the full dataset must be retrieved from a single provider.",
     correct: true,
-    feedback: "Great job!"
+    feedback: "You spotted the false statement! Because each node in a DAG has its own CID, it can be shared and retrieved independently of a larger DAG in which it's embedded."
   },
   {
-    answer: "Some incorrect answer",
+    answer: "Different segments of a dataset can be retrieved in parallel from a variety of providers or data centers around the globe.",
     correct: false,
-    feedback: "Oops. Here's some clue about why that answer is wrong."
-  },
-  {
-    answer: "Some incorrect answer",
-    correct: false,
-    feedback: "Sorry, here's some clue about why that answer is wrong."
+    feedback: "Oops! This statement is actually true. Distributability is one of the key benefits of Merkle DAGs. Can you find the statement that's false?"
   }
 ]
 
