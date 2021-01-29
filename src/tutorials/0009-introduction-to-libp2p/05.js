d798c3c509c157ea5edda103b4aee8c3cbdc6a85
terichadbourne <terichad@gmail.com>
2021-01-29 15:24:25 -0500

    fix casing issues

--- src/tutorials/0009-introduction-to-libp2p/05.js
+++ src/tutorials/0009-introduction-to-libp2p/05.js
@@ -9,12 +9,12 @@ const choices = [
     feedback: "That's quite not right. Try again."
   },
   {
-    answer: "`libp2p` supports a lot of environments: javascript (both NodeJs and Browsers), Go lang, Rust, Python, JVM, C++, and Nim.",
+    answer: "`libp2p` supports a lot of environments: JavaScript (both Node.js and Browsers), Go lang, Rust, Python, JVM, C++, and Nim.",
     correct: true,
     feedback: "That's right!"
   },
   {
-    answer: "`libp2p` does not support many environments besides NodeJs. Browser support is in the works.",
+    answer: "`libp2p` does not support many environments besides Node.js. Browser support is in the works.",
     correct: false,
     feedback: "Sorry, but that's not quite right."
   }
