b9e154f98c1f8ff5a4e20d3cb4297f663d294e91
Yusef Napora <yusef@napora.org>
2021-02-23 13:09:58 -0500

    make dummy tests pass

--- src/tutorials/0010-hands-on-with-libp2p/03.js
+++ src/tutorials/0010-hands-on-with-libp2p/03.js
@@ -1,19 +1,5 @@
 const validate = async (result, ipfs) => {
-  if (result) {
-    return { success: 'Happy Message!' }
-  } else {
-    return { fail: 'Sad but useful message :(' }
-  }
-
-  /*
-    There are some additional options you can find useful:
-
-    If you want to show some data or result to the user, it's possible to add an additional step after submitting the code:
-    https://github.com/ProtoSchool/protoschool.github.io/blob/main/README.md#display-results-to-the-user-optional
-
-    If you want to catch external errors and override them to display a more user-friendly error message:
-    https://github.com/ProtoSchool/protoschool.github.io/blob/main/README.md#override-external-error-messages-optional
-  */
+  return { success: 'Validation coming soon!' }
 }
 
 const code = `/* global ipfs, all, toBuffer */
