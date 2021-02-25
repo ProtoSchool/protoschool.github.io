f7908b4530e31e834ce062db99e6c539643eaa73
terichadbourne <terichad@gmail.com>
2021-02-25 16:07:09 -0500

    enable translations for footer

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -11,7 +11,7 @@
         </router-link>
       </div>
         <div class="w-20-l w-25-m w-33 column">
-          <span class="fw7">Courses</span>
+          <span class="fw7">{{translations.columnHeaders.courses}}</span>
           <ul class="list pl0">
             <li class="pv1"><a class="link underline-hover white o-80 glow" href="/course/ipfs">IPFS</a></li>
             <li class="pv1"><a class="link underline-hover white o-80 glow" href="/course/filecoin">Filecoin</a></li>
@@ -20,37 +20,38 @@
           </ul>
         </div>
         <div class="w-20-l w-25-m w-33 column">
-          <span class="fw7">Community</span>
+          <span class="fw7">{{translations.columnHeaders.community}}</span>
           <ul class="list pl0">
-            <li class="pv1"><a class="link underline-hover white o-80 glow" href="https://github.com/protoschool">GitHub</a></li>
-            <li class="pv1"><a class="link underline-hover white o-80 glow" href="https://twitter.com/protoschool">Twitter</a></li>
-            <li class="pv1"><a class="link underline-hover white o-80 glow" href="/news">Newsletter</a></li>
-            <li class="pv1"><a class="link underline-hover white o-80 glow" href="/contribute">Contribute</a></li>
-            <li class="pv1"><a class="link underline-hover white o-80 glow" href="/contribute#contact">Contact</a></li>
+            <li class="pv1"><a class="link underline-hover white o-80 glow" target="_blank" href="https://github.com/protoschool">GitHub</a></li>
+            <li class="pv1"><a class="link underline-hover white o-80 glow" target="_blank" href="https://twitter.com/protoschool">Twitter</a></li>
+            <li class="pv1"><a class="link underline-hover white o-80 glow" href="/news">{{translations.communityLinks.newsletter}}</a></li>
+            <li class="pv1"><a class="link underline-hover white o-80 glow" href="/contribute">{{translations.communityLinks.contribute}}</a></li>
+            <li class="pv1"><a class="link underline-hover white o-80 glow" href="/contribute#contact">{{translations.communityLinks.contact}}</a></li>
           </ul>
         </div>
 
         <div class="w-20-l w-25-m w-33 column">
-          <span class="fw7">Related Projects</span>
+          <span class="fw7">{{translations.columnHeaders.relatedProjects}}</span>
           <ul class="list pl0">
-            <li class="pv1"><a class="link underline-hover white o-80 glow" href="https://ipfs.io">IPFS</a></li>
-            <li class="pv1"><a class="link underline-hover white o-80 glow" href="https://filecoin.io">Filecoin</a></li>
-            <li class="pv1"><a class="link underline-hover white o-80 glow" href="https://ipld.io">IPLD</a></li>
-            <li class="pv1"><a class="link underline-hover white o-80 glow" href="https://libp2p.io">libp2p</a></li>
-            <li class="pv1"><a class="link underline-hover white o-80 glow" href="https://multiformats.io">Multiformats</a></li>
+            <li class="pv1"><a class="link underline-hover white o-80 glow" target="_blank" href="https://ipfs.io">IPFS</a></li>
+            <li class="pv1"><a class="link underline-hover white o-80 glow" target="_blank" href="https://filecoin.io">Filecoin</a></li>
+            <li class="pv1"><a class="link underline-hover white o-80 glow" target="_blank" href="https://ipld.io">IPLD</a></li>
+            <li class="pv1"><a class="link underline-hover white o-80 glow" target="_blank" href="https://libp2p.io">libp2p</a></li>
+            <li class="pv1"><a class="link underline-hover white o-80 glow" target="_blank" href="https://multiformats.io">Multiformats</a></li>
           </ul>
         </div>
     </div>
     <div class="flex justify-start mt4 mw7 center ph3 f6 o-70"> <!-- fake element plus copyright -->
       <div class="w-33-l">
       </div>
-      <p>© <a class="link underline white o-80 glow" href="https://protocol.ai">Protocol Labs</a> | Except as <a class="link underline white o-80 glow" href="https://protocol.ai/legal/">noted</a>, content licensed <a class="link underline white o-80 glow" href="https://creativecommons.org/licenses/by/3.0/">CC-BY 3.0</a>.</p>
+      <p>© <a class="link underline white o-80 glow" target="_blank" href="https://protocol.ai">Protocol Labs</a> | {{translations.copyright._1}} <a class="link underline white o-80 glow" target="_blank" href="https://protocol.ai/legal/">{{translations.copyright._2}}</a>{{translations.copyright._3}} <a class="link underline white o-80 glow" target="_blank" v-bind:href="translations.copyright.licenseURL" >CC-BY 3.0</a>{{translations.copyright._4}}</p>
     </div>
   </footer>
 </template>
 
 <script>
 import ProtoSchoolLogo from '../../static/images/ps_symbol_color.svg?inline'
+import translations from '../../static/translations'
 
 export default {
   name: 'Footer',
@@ -59,12 +60,16 @@ export default {
   },
   props: {
     noTopMargin: Boolean
+  },
+  computed: {
+    translations: function () {
+      return translations.footer
+    }
   }
 }
 </script>
 <style scoped>
 .column {
-  /* margin-top: 3.44rem; */
   margin-top: 0.7rem;
 }
 </style>
